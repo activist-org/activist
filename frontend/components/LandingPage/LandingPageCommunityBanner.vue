ac35231516b7b69a1b8d6c9d7a6de84d26f3b24a
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-01-25 16:16:04 +0100

    Text color edits to the community section of the splash

--- frontend/components/LandingPage/LandingPageCommunityBanner.vue
+++ frontend/components/LandingPage/LandingPageCommunityBanner.vue
@@ -1,26 +1,26 @@
 <template>
   <div
-    class="flex flex-col w-full py-6 space-y-4 bg-light-highlight dark:bg-dark-highlight text-light-text dark:text-dark-text"
+    class="flex flex-col w-full py-6 space-y-4 bg-light-highlight dark:bg-dark-highlight text-light-special-text dark:text-dark-special-text"
   >
-    <p class="text-xl font-semibold text-center">{{ $t("join-our-community") }}</p>
+    <p class="text-xl font-semibold text-center text-light-text dark:text-dark-text">{{ $t("join-our-community") }}</p>
     <div class="flex text-xl justify-center space-x-12 text-med">
       <a
         href="https://github.com/activist-org/activist"
-        class="flex space-x-3 items-center hover:text-black dark:hover:text-white"
+        class="flex space-x-3 items-center hover:text-light-text dark:hover:text-dark-text"
       >
         <Icon name="bi:github" size="1em" />
         <p>GitHub</p>
       </a>
       <a
         href="https://twitter.com/activist_org"
-        class="flex space-x-3 items-center hover:text-black dark:hover:text-white"
+        class="flex space-x-3 items-center hover:text-light-text dark:hover:text-dark-text"
       >
         <Icon name="bi:twitter" size="1em" />
         <p>Twitter</p>
       </a>
       <a
         href="https://instagram.com/activist_org"
-        class="flex space-x-3 items-center hover:text-black dark:hover:text-white"
+        class="flex space-x-3 items-center hover:text-light-text dark:hover:text-dark-text"
       >
         <Icon name="bi:instagram" size="1em" />
         <p>Instagram</p>
