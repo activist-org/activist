ad39674145935f832d77a5a7c58dc1f6d334c0e1
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-01-29 15:20:51 +0100

    #27 adapt drop downs with headless UI for cross-browser support

--- frontend/components/ThemeSelector.vue
+++ frontend/components/ThemeSelector.vue
@@ -1,49 +1,58 @@
 <template>
-  <div>
-    <button
-      class="px-3 py-2 font-medium border rounded-md peer text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content border-light-text dark:border-dark-text focus:border-light-cta-orange dark:focus:border-dark-cta-orange hover:bg-light-highlight dark:hover:bg-dark-highlight focus:hover:bg-light-content dark:focus:hover:bg-dark-content"
-    >
-      <div class="flex items-center text-sm space-x-2">
-        <Icon v-if="$colorMode.preference == 'system'" name="bi:circle-half" />
-        <Icon v-if="$colorMode.preference == 'light'" name="bi:sun" />
-        <Icon v-else-if="$colorMode.preference == 'dark'" name="bi:moon" />
-        <p class="hidden lg:block">Theme</p>
-        <p></p>
-        <Icon name="bi:chevron-down" />
-      </div>
-    </button>
-    <div class="invisible duration-200 -translate-y-1 peer-focus:visible">
-      <div
-        class="absolute right-0 mt-3 overflow-hidden text-sm border rounded-md xl:text-base text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content border-light-text dark:border-dark-text"
-      >
-        <div class="flex-col">
-            <button
-              class="flex items-center w-full p-1 pl-2 border-l-4 border-transparent xl:p-2 xl:pl-4 hover:border-light-cta-orange dark:hover:border-dark-cta-orange hover:bg-light-distinct dark:hover:bg-dark-distinct"
-              @click="$colorMode.preference = 'system'"
-            >
-              <Icon name="bi:circle-half" size="1em" />
-              <p class="pl-2 pr-4">System</p>
-            </button>
-            <button
-              class="flex items-center w-full p-1 pl-2 border-l-4 border-transparent xl:p-2 xl:pl-4 hover:border-light-cta-orange dark:hover:border-dark-cta-orange hover:bg-light-distinct dark:hover:bg-dark-distinct"
-              @click="$colorMode.preference = 'light'"
-            >
-              <Icon name="bi:sun" size="1.1em" />
-              <p class="pl-2 pr-4">Light</p>
-            </button>
-            <button
-              class="flex items-center w-full p-1 pl-2 border-l-4 border-transparent xl:p-2 xl:pl-4 hover:border-light-cta-orange dark:hover:border-dark-cta-orange hover:bg-light-distinct dark:hover:bg-dark-distinct"
-              @click="$colorMode.preference = 'dark'"
-            >
-              <Icon name="bi:moon" size="1em" />
-              <p class="pl-2 pr-4">Dark</p>
-            </button>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton
+        class="inline-flex w-full justify-center rounded-md px-4 py-2 font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-light-cta-orange dark:focus-visible:ring-dark-cta-orange focus-visible:ring-opacity-75 text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content hover:bg-light-highlight dark:hover:bg-dark-highlight">
+        <div class="flex items-center text-sm space-x-2">
+          <Icon v-if="$colorMode.preference == 'system'" name="bi:circle-half" />
+          <Icon v-if="$colorMode.preference == 'light'" name="bi:sun" />
+          <Icon v-else-if="$colorMode.preference == 'dark'" name="bi:moon" />
+          <p class="hidden lg:block">Theme</p>
+          <p></p>
+          <Icon name="bi:chevron-down" />
         </div>
-      </div>
+      </MenuButton>
     </div>
-  </div>
+
+    <transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+      <MenuItems
+        class="absolute right-0 mt-2 origin-top-right divide-y border rounded-md bg-light-content dark:bg-dark-content shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none border-light-text dark:border-dark-text">
+        <div class="px-2 py-2">
+          <MenuItem v-slot="{ active }">
+          <button :class="[
+            active ? 'bg-light-cta-orange dark:bg-dark-cta-orange text-light-content dark:text-dark-content' : 'text-light-text dark:text-dark-text',
+            'group flex w-full items-center rounded-md pl-4 pr-2 py-2 text-sm',
+          ]" @click="$colorMode.preference = 'system'">
+            <Icon name="bi:circle-half" size="1em" />
+            <p class="pl-2 pr-2">System</p>
+          </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+          <button :class="[
+            active ? 'bg-light-cta-orange dark:bg-dark-cta-orange text-light-content dark:text-dark-content' : 'text-light-text dark:text-dark-text',
+            'group flex w-full items-center rounded-md pl-4 pr-2 py-2 text-sm',
+          ]" @click="$colorMode.preference = 'light'">
+            <Icon name="bi:sun" size="1.1em" />
+            <p class="pl-2 pr-2">Light</p>
+          </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+          <button :class="[
+            active ? 'bg-light-cta-orange dark:bg-dark-cta-orange text-light-content dark:text-dark-content' : 'text-light-text dark:text-dark-text',
+            'group flex w-full items-center rounded-md pl-4 pr-2 py-2 text-sm',
+          ]" @click="$colorMode.preference = 'dark'">
+            <Icon name="bi:moon" size="1em" />
+            <p class="pl-2 pr-2">Dark</p>
+          </button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
 </template>
 
 <script setup>
-const colorMode = useColorMode();
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 </script>
