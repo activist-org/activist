f9e88e712a958777537a485488f6d09524081b5a
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-02-05 15:19:51 +0100

    #50 add baseline files for sidebar

---
+++ frontend/components/Sidebar/Sidebar.vue
@@ -0,0 +1,6 @@
+<template>
+  <SidebarHeader />
+  <SearchBar />
+  <SidebarMainSectionSelector />
+  <SidebarFooter />
+</template>
---
+++ frontend/components/Sidebar/SidebarHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="relative z-0 w-24 h-6">
+    <LogosActivist
+      class="absolute inset-0 flex items-center justify-center z-1 overflow-clip"
+    />
+  </div>
+</template>
