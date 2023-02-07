5e719d611b1938e80ffba620353ff79547eb3dc7
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-02-07 21:01:48 +0100

    #48 update of meta tags and adding OG images

--- frontend/components/LandingPage/LandingPageSplash.vue
+++ frontend/components/LandingPage/LandingPageSplash.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="flex-1 w-full pt-8 pb-6 bg-light-distinct dark:bg-dark-distinct text-light-text dark:text-dark-text sm:pt-12 sm:pb-10 md:pt-16 md:pb-12 lg:pb-14 lg:pt-20 xl:pt-24 xl:pb-16 2xl:pt-28 2xl:pb-18 space-y-4 sm:space-y-9 xl:space-y-10"
+    class="flex-1 w-full pt-10 pb-8 bg-light-distinct dark:bg-dark-distinct text-light-text dark:text-dark-text sm:pt-12 sm:pb-10 md:pt-16 md:pb-12 lg:pb-14 lg:pt-20 xl:pt-24 xl:pb-16 2xl:pt-28 2xl:pb-18 space-y-4 sm:space-y-9 xl:space-y-10"
   >
     <div class="flex justify-center">
-      <p class="w-10/12 text-2xl font-semibold leading-snug text-center lg:font-bold sm:text-3xl md:text-4xl xl:text-5xl md:max-w-lg xl:max-w-2xl sm:leading-snug md:leading-snug xl:leading-snug">
+      <p
+        class="w-10/12 text-2xl font-semibold leading-snug text-center lg:font-bold sm:text-3xl md:text-4xl xl:text-5xl md:max-w-lg xl:max-w-2xl sm:leading-snug md:leading-snug xl:leading-snug"
+      >
         {{ $t("landing-header") }}
       </p>
     </div>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -77,17 +77,70 @@ export default defineNuxtConfig({
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       title: "activist",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Open-source, nonprofit activism platform.",
+        },
+        { property: "og:site_name", content: "activist" },
+        { hid: "og:type", property: "og:type", content: "website" },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "https://activist.org",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "activist",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Open-source, nonprofit activism platform.",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "/images/activistOGImage.png",
+        },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:image:height", content: "630" },
+
+        { name: "twitter:site", content: "@activist_org" },
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          hid: "twitter:url",
+          name: "twitter:url",
+          content: "https://activist.org",
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: "activist",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: "Open-source, nonprofit activism platform.",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "/images/activistTwitterOGImage.png",
+        },
+      ],
       link: [
         {
           rel: "icon",
           type: "image/svg+xml",
           href: "/favicon.svg",
         },
-      ],
-      meta: [
         {
-          name: "activist",
-          content: "An open-source, nonprofit political activism network.",
+          hid: "canonical",
+          rel: "canonical",
+          href: "https://activist.org",
         },
       ],
     },
