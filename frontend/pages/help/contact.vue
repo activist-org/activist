658fbbf65afa931cde60973be81582b9e0338aa2
5ud03r5 <kmarek973@gmail.com>
2023-03-23 11:44:11 +0100

    feature: Added mailing function, installed new package to handle emails nuxt-mail, added "thank you" message, button disabled when form not completed

--- frontend/pages/help/contact.vue
+++ frontend/pages/help/contact.vue
@@ -5,7 +5,7 @@
       <Title>{{ title }}</Title>
     </Head>
     <div class="container mx-auto md:w-[1200px]">
-      <div class="flex container max-md:flex-col mx-auto justify-center p-4">
+      <div v-if="!emailSent" class="flex container max-md:flex-col mx-auto justify-center p-4">
         <div class="md:w-1/2 flex flex-col space-y-2 p-4 max-md:text-center">
           <h1 class="text-light-text dark:text-dark-text text-[40px]">
             We'd love to hear from you!
@@ -44,7 +44,7 @@
 
             <label :class="!messageValidated ? 'text-red-500' : 'text-light-text dark:text-dark-text'">Message
               <span v-if="!messageValidated">cannot be
-                empty</span></label>
+                empty.</span></label>
 
             <textarea @blur="validateMessage" v-model="message"
               :class="!messageValidated ? 'outline-red-500 outline outline-2' : 'outline-none focus:outline-none'"
@@ -52,18 +52,28 @@
               class="p-2 rounded-md placeholder:dark:dark-placeholder placeholder:light-placeholder placeholder:italic resize-none bg-light-highlight dark:bg-dark-highlight focus:bg-light-distinct focus:dark:bg-dark-distinct text-light-text dark:text-dark-text"
               autocomplete="off" spellcheck="false"></textarea>
 
-            <button type="submit" class="ml-auto bg-light-cta-orange dark:bg-dark-cta-orange hover:bg-light-cta-orange-light 
-                    active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-light dark:active:bg-dark-cta-orange 
-                    px-4 py-2 font-medium text-center border rounded-md select-none xl:rounded-lg text-light-distinct 
-                    border-light-distinct dark:text-dark-distinct dark:border-dark-distinct focus-brand">Send</button>
+            <button type="submit"
+              class="ml-auto px-4 py-2 font-medium text-center border rounded-md select-none xl:rounded-lg focus-brand bg-light-cta-orange dark:bg-dark-cta-orange text-light-distinct  border-light-distinct dark:text-dark-distinct dark:border-dark-distinct"
+              :class="!buttonDisabled ? 'hover:bg-light-cta-orange-light active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-light dark:active:bg-dark-cta-orange  ' : 'cursor-not-allowed'"
+              :disabled="buttonDisabled">Send</button>
           </form>
         </div>
+      </div>
+      <div v-else class="p-4 justify-center flex text-center flex-col mx-auto items-center">
+        <h1 class="text-[40px] font-semibold ">Thank you for for contacting Activist team</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+          a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+          of Lorem Ipsum.</p>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+
 const title = ref("Contact");
 const name = ref('');
 const email = ref('');
@@ -71,6 +81,9 @@ const message = ref('');
 const nameValidated = ref(true)
 const emailValidated = ref(true)
 const messageValidated = ref(true)
+const buttonDisabled = ref(true)
+const mail = useMail()
+const emailSent = ref(false)
 
 const validateEmail = () => {
   if (!email.value.match(/.*@\w+\.\w+/)) {
@@ -107,12 +120,22 @@ watch(message, () => {
   }
 })
 
-const sendEmail = () => {
+watch([message, email, name], () => {
   if (name.value.trim().length > 0 && email.value.match(/.*@\w+\.\w+/) && message.value.trim().length > 0) {
-    //... email send function
-    navigateTo('/')
+    buttonDisabled.value = false
   } else {
+    buttonDisabled.value = true
+  }
+})
 
+const sendEmail = async () => {
+  if (name.value.trim().length > 0 && email.value.match(/.*@\w+\.\w+/) && message.value.trim().length > 0) {
+    await mail.send({
+      from: email.value,
+      subject: `Activist contact message from ${name.value}`,
+      text: message.value,
+    })
+    emailSent.value = true
   }
 }
 
