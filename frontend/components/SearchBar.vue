81ecedcbef2516a1197a5c75983c8877120a50a0
muditagarwalna@gmail.comx <muditagarwalna@gmail.com>
2023-04-14 14:38:05 +0530

    Added / hotkey to search bar

--- frontend/components/SearchBar.vue
+++ frontend/components/SearchBar.vue
@@ -9,6 +9,7 @@
     <Icon name="bi:search" size="1em" class="my-1" />
     <input
       v-if="sidebar.collapsed == false || sidebar.collapsedSwitch == false"
+      ref="input"
       class="w-16 bg-transparent outline-none"
       type="text"
       placeholder="Search"
@@ -41,7 +42,20 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { useMagicKeys, whenever } from "@vueuse/core";
 const sidebar = useSidebar();
+const input = ref();
+const keys = useMagicKeys();
+
+whenever(keys.slash, () => {
+  input.value.focus();
+  setTimeout(() => {
+    if (input.value.value.startsWith("/")) {
+      input.value.value = "";
+    }
+  }, 0);
+});
 </script>
 
 <style>
