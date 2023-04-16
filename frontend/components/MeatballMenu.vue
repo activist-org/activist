77fbc2949bdcd0b693f62c5c149ca889e4f6b981
purupurupu <purupurupu0prog@gmail.com>
2023-04-16 23:36:39 +0900

    Close meatball menu #49

--- frontend/components/MeatballMenu.vue
+++ frontend/components/MeatballMenu.vue
@@ -55,27 +55,27 @@
               />
             </DisclosureButton>
             <DisclosurePanel class="px-3 py-2">
-              <button
+              <MenuButton
                 class="flex items-center w-full px-2 py-2 text-sm rounded-md group text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight"
                 @click="$colorMode.preference = 'system'"
               >
                 <Icon name="bi:circle-half" size="1em" />
                 <p class="px-1">System</p>
-              </button>
-              <button
+              </MenuButton>
+              <MenuButton
                 class="flex items-center w-full px-2 py-2 text-sm rounded-md group text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight"
                 @click="$colorMode.preference = 'light'"
               >
                 <Icon name="bi:sun" size="1em" />
                 <p class="px-1">Light</p>
-              </button>
-              <button
+              </MenuButton>
+              <MenuButton
                 class="flex items-center w-full px-2 py-2 text-sm rounded-md group text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight"
                 @click="$colorMode.preference = 'dark'"
               >
                 <Icon name="bi:moon" size="1em" />
                 <p class="px-1">Dark</p>
-              </button>
+              </MenuButton>
             </DisclosurePanel>
           </Disclosure>
           <Disclosure v-slot="{ open }">
@@ -109,11 +109,13 @@
                   :key="locale.code"
                   :to="switchLocalePath(locale.code)"
                 >
-                  <li
-                    class="flex w-24 px-1 py-2 text-sm rounded-md group items-left text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight"
-                  >
-                    {{ locale.name }}
-                  </li>
+                  <MenuButton>
+                    <li
+                      class="flex w-24 px-1 py-2 text-sm rounded-md group items-left text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight"
+                    >
+                      {{ locale.name }}
+                    </li>
+                  </MenuButton>
                 </NuxtLink>
               </ul>
             </DisclosurePanel>
