fe12518b4df1331982a9a626d63fa9836935f8a6
Zwnow <svenotimm@gmail.com>
2023-05-05 14:40:49 +0200

    changed X button to icon

--- frontend/components/PopupImage.vue
+++ frontend/components/PopupImage.vue
@@ -1,11 +1,14 @@
 <template>
     <div
     id="popup" 
-    class="fixed hidden flex flex-col top-0 items-center w-full h-screen z-10 overflow-hidden bg-light-popup dark:bg-dark-popup">
+    class="fixed hidden flex flex-col top-0 items-center w-full h-screen z-10 overflow-hidden bg-light-popup dark:bg-dark-popup cursor-pointer">
         <div id="popup-header" class="flex flex-row items-end w-full justify-end">
         </div>
         <div class="w-4/5 flex p-4 flex-col justify-center items-center">
-            <label class="px-4 py-2 font-medium text-center border select-none rounded-md xl:rounded-lg text-light-distinct border-light-distinct dark:text-dark-distinct dark:border-dark-distinct focus-brand bg-light-cta-orange dark:bg-dark-cta-orange hover:bg-light-cta-orange-light dark:hover:bg-dark-cta-orange-light text-sm hidden lg:block cursor-pointer self-end">⨯</label>
+            
+            <Icon 
+            name="bi:x-circle-fill" 
+            class="self-end w-4 h-4 light-highlight dark:dark-highlight"/>
             <img 
             id="popup-image"
             src=""
