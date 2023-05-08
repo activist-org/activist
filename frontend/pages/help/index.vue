e40af15c481a16742ed76004c886db80bbc81343
jpw3667 <jbetberg@gmail.com>
2023-05-07 20:22:44 -0400

    Updated the help page

--- frontend/pages/help/index.vue
+++ frontend/pages/help/index.vue
@@ -4,11 +4,11 @@
       <Title>{{ title }}</Title>
     </Head>
     <h1 class="p-4 text-light-text dark:text-dark-text">
-      Welcome to the help page prototype
-      <a href ="https://activist.org/en/help/faq/" style ="color:#FFFFFF;"> FAQ</a> <br>
-      <a href ="https://activist.org/en/help/contact/" style ="color:#FFFFFF;">Contact</a> <br>
-      <a href ="https://activist.org/en/help/press/" style ="color:#FFFFFF;">Press</a> <br>
-      <a href ="https://activist.org/en/help/status/" style ="color:#FFFFFF;">Status</a>
+      Welcome to the help page prototype <br>
+      <a href ="https://activist.org/en/help/faq/" style ="color:##0000EE;"><u>FAQ</u></a> <br>
+      <a href ="https://activist.org/en/help/contact/" style ="color:##0000EE;"><u>Contact</u></a> <br>
+      <a href ="https://activist.org/en/help/press/" style ="color:##0000EE;"><u>Press</u></a> <br>
+      <a href ="https://activist.org/en/help/status/" style ="color:##0000EE;"><u>Status</u></a>
     </h1>
   </div>
 </template>
