6fff0d7fd5032c3eae4b159f0f67d17fb80706e9
jpw3667 <jbetberg@gmail.com>
2023-05-07 22:02:42 -0400

    updated help page

--- frontend/pages/help/index.vue
+++ frontend/pages/help/index.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
+  <div class="text-light-text dark:text-dark-text">
     <Head>
-      <Title>{{ title }}</Title>
+      <Title>{{ help }}</Title>
     </Head>
-    <h1 class="p-4 text-light-text dark:text-dark-text">
-      Welcome to the help page prototype <br>
-      <a href ="https://activist.org/en/help/faq/" style ="color:##0000EE;"><u>FAQ</u></a> <br>
-      <a href ="https://activist.org/en/help/contact/" style ="color:##0000EE;"><u>Contact</u></a> <br>
-      <a href ="https://activist.org/en/help/press/" style ="color:##0000EE;"><u>Press</u></a> <br>
-      <a href ="https://activist.org/en/help/status/" style ="color:##0000EE;"><u>Status</u></a>
-    </h1>
+    <div
+      class="flex flex-col items-center w-full responsive-py-4 responsive-px-5"
+    >
+      <div class="grid w-10/12 grid-cols-1 md:w-full 2xl:grid-cols-2">
+        <div class="items-center space-y-4 text-left md:items-start">
+          <!-- Note: image floating right of content. -->
+          <h1 class="pb-2 font-semibold responsive-text-4">
+            Get help here
+          </h1>
+          <p>Frequently asked questions 
+          <a href="https://activist.org/en/help/faq/" style="color:##0000EE;"><u>here</u></a> <br>
+          Want to contact our team? Try  <a href="https://activist.org/en/help/Contact/" style="color:##0000EE;"><u>here</u></a> <br>
+          Contact our press? Try  <a href="https://activist.org/en/help/press/" style="color:##0000EE;"><u>here</u></a> <br>
+          Check the status of our server <a href="https://activist.org/en/help/status/" style="color:##0000EE;"><u>here</u></a>
+          </p>
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
