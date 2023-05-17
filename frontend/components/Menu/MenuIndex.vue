9813b9a8468cea3e8859b6be7f7c997b5ddf9060
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-05-17 19:05:53 +0200

    #85 add baseline component for sidebar menu

--- frontend/components/Menu/MenuIndex.vue
+++ frontend/components/Menu/MenuIndex.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <div class="flex flex-col items-center">
+    <!-- TODO: Replace icon below with images.-->
+    <Icon name="radix-icons:component-placeholder" size="2em" />
+    <p>Name</p>
+    <!-- TODO: Replace with a list that adds the buttons.-->
+    <ul class="flex flex-col"></ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name?: string;
+  menuType?: string; // `organization`, `event`, etc
+}>();
+</script>
