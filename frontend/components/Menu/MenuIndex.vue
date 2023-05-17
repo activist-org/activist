249a5f8cef6aab4db77bac5df47a15411887ead2
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-05-17 19:08:08 +0200

    #85 color responsive text for sidebar menu

--- frontend/components/Menu/MenuIndex.vue
+++ frontend/components/Menu/MenuIndex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center text-light-text dark:text-dark-text">
     <!-- TODO: Replace icon below with images.-->
     <Icon name="radix-icons:component-placeholder" size="2em" />
     <p>Name</p>
