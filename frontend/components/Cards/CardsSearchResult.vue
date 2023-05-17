0ed2984f9502bfe1dc5f5d3e50abc48c6b57f28e
James Jenkinson <james@ctrl.alt.coop>
2023-05-17 22:27:57 +0200

    Add organization search result card

--- frontend/components/Cards/CardsSearchResult.vue
+++ frontend/components/Cards/CardsSearchResult.vue
@@ -2,19 +2,48 @@
   <div
     class="flex flex-col items-center w-full h-min card-style sm:items-start"
   >
-    <div class="px-1 py-1 sm:px-2 sm:py-2">
-      <p>{{ name }}</p>
-      <p>{{ description }}</p>
-      <p>This search result is an {{ searchResultType }}</p>
+    <div class="px-1 py-1 sm:px-2 sm:py-2 flex">
+      <img class="search-result-image" src="/images/tech-from-below.svg" alt="This is the alt text, please translate"/>
+      <div class="py-5 px-8">
+        <h2 class="font-bold text-3xl mb-5">{{ name }}</h2>
+        <TopicMarker class="mb-5" :topic="organization.topic"></TopicMarker>
+        <div class="flex gap-4 mb-4 text-xl" v-if="organization">
+          <div>
+            <Icon name="bi:map" class="mr-1" />
+            {{  organization.location }}</div>
+          <div>
+            <Icon name="bi:people" class="mr-1" />
+            {{  organization.members }} {{ $t('members') }}
+          </div>
+          <div>
+            <Icon name="bi:balloon-heart" class="mr-1" />
+            {{  organization.supporters }} {{ $t('supporters') }}
+          </div>
+        </div>
+        <div class="text-xl">
+          {{ organization.description }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Organization } from '../../types/organization'
+
 defineProps<{
   name: string;
   description: string;
   searchResultType: string; // `organization`, `event`, etc
   private?: boolean;
+  mode: 'organization' | 'event' | 'resource'
+  organization: Organization
 }>();
 </script>
+
+<style scoped>
+.search-result-image {
+  height: 250px;
+  width: 250px;
+}
+</style>
--- frontend/components/TopicMarker.vue
+++ frontend/components/TopicMarker.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="topic-marker w-fit h-7 p-2 py-3 flex justify-between items-center rounded-full border bg-light-cta-orange/40 text-light-cta-orange border-light-cta-orange dark:bg-dark-cta-orange/50 dark:border-dark-cta-orange dark:text-dark-cta-orange"
+    class="topic-marker w-fit h-7 p-2 py-1 px-3 flex justify-between items-center rounded-full border bg-light-cta-orange/40 text-light-cta-orange border-light-cta-orange dark:bg-dark-cta-orange/50 dark:border-dark-cta-orange dark:text-dark-cta-orange"
   >
     <Icon
       name="bi:globe"
       size="1em"
       class="flex-shrink-0 w-5 h-5 my-1 rounded-full"
     />
-    <p class="text-base text-center pl-2 font-bold">{{ topic }}</p>
+    <p class="text-2xl text-center pl-2 font-bold">{{ topic }}</p>
   </div>
 </template>
 
