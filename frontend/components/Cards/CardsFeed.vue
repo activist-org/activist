67097100fad744c9ea8b5f861d6066c85d348fdc
momanyisamuel <momanyisamuel48@gmail.com>
2023-05-18 20:21:11 +0300

    Feed card component #142

--- frontend/components/Cards/CardsFeed.vue
+++ frontend/components/Cards/CardsFeed.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="rounded-md bg-light-header">
+  <div class="rounded-md bg-light-header dark:bg-dark-header">
     <div class="px-2 py-2 sm:px-5 sm:py-5">
       <div class="flex items-center justify-start gap-1 w-full mb-3">
         <Icon v-if="iconName" :name="iconName" />
-        <h1 class="text-xl font-bold">{{ title }}</h1>
+        <h1 class="responsive-text-1 font-bold">{{ title }}</h1>
       </div>
-      <p class="text-xl font-semibold pb-3">
+      <p class="responsive-text-0 font-base pb-3">
         {{ description }}
       </p>
-      <div class="w-full bg-light-section-div h-40 rounded-md">
+      <div class="w-full bg-light-section-div dark:bg-dark-section-div h-40 rounded-md">
         <img src="#" alt="" class="" />
       </div>
     </div>
