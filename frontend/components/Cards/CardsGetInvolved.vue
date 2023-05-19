0081949261303dd95214333d93daecbda564ae08
momanyisamuel <momanyisamuel48@gmail.com>
2023-05-19 23:44:04 +0300

    Get involved card component #143

--- frontend/components/Cards/CardsGetInvolved.vue
+++ frontend/components/Cards/CardsGetInvolved.vue
@@ -12,35 +12,91 @@
         />
       </h1>
     </div>
-    <div class="flex flex-col sm:flex-row justify-between items-center px-4">
-      <p class="mb-1 text-center sm:text-justify sm:mb-0">The following are working groups within Berliner Obdachlosenhilfe:</p>
-      <div class="flex items-center justify-center gap-3">
-        <LabeledBtn
-          :cta="true"
-          label="View all groups"
-          linkTo="https://tally.so/r/nprxbq"
-          fontSize="sm"
-          class=""
-        />
-        <LabeledBtn
-          :cta="true"
-          label="Join organisation"
-          linkTo="https://tally.so/r/nprxbq"
-          fontSize="sm"
-        />
+    <div v-if="feeds">
+      <div class="flex flex-col sm:flex-row justify-between items-center px-4">
+        <p class="mb-1 text-center sm:text-justify sm:mb-0">
+          The following are working groups within Berliner Obdachlosenhilfe:
+        </p>
+        <div class="flex items-center justify-center gap-3">
+          <LabeledBtn
+            :cta="true"
+            label="View all groups"
+            linkTo="https://tally.so/r/nprxbq"
+            fontSize="sm"
+            class=""
+          />
+          <LabeledBtn
+            :cta="true"
+            label="Join organisation"
+            linkTo="https://tally.so/r/nprxbq"
+            fontSize="sm"
+          />
+        </div>
+      </div>
+      <div
+        class="w-full flex flex-col items-center justify-center sm:flex-row sm:justify-between sm:items-start sm:space-x-3 overflow-x-scroll scrolling-touch p-4 mb-4"
+      >
+        <div v-for="feed in feeds">
+          <CardsFeed
+            :title="feed.title"
+            :description="feed.description"
+            class="mb-3 sm:mb-0 w-[310px]"
+          />
+        </div>
       </div>
     </div>
-    <div
-      class="w-full flex flex-col items-center justify-center sm:flex-row sm:justify-between sm:items-start sm:space-x-3 overflow-x-scroll p-4 mb-4"
-    >
-      <div v-for="feed in feeds">
-        <CardsFeed :title="feed.title" :description="feed.description" class="mb-3 sm:mb-0" />
+    <div class="px-4" v-if="event">
+      <div>
+        <p class="mb-1 text-center sm:text-justify sm:mb-2">
+          {{ event?.description }}
+        </p>
+        <p class="mb-1 text-center sm:text-justify sm:mb-2">
+          Detailed shift plan:
+        </p>
+        <ul class="mb-1 text-center sm:text-justify sm:mb-2 list-disc" v-for="(plan, index) in event?.plans">
+          <li class="ml-6">{{ plan }}</li>
+        </ul>
+        <p class="mb-1 text-center sm:text-justify sm:mb-2">
+          We’d be happy to have your support!
+        </p>
+      </div>
+      <div class="flex flex-col justify-center">
+        <p class="mb-1 text-center sm:text-justify sm:mb-2">
+          Please read the legal disclaimer below for more information on your
+          rights during this event!
+        </p>
+        <div
+          class="flex px-2 py-3 w-full bg-light-header dark:bg-dark-header cursor-pointer justify-start items-center gap-3 border rounded-md font-md group text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight focus-brand"
+          :class="{
+            'bg-light-menu-selection dark:bg-dark-menu-selection text-light-distinct dark:text-dark-distinct':
+              selected == true,
+            'text-light-text dark:text-dark-text': selected == false,
+          }"
+        >
+          <Icon name="bi:globe" />
+          <p>Legal disclaimer</p>
+          <Icon name="bi:globe" />
+        </div>
+        <div class="mb-6 mt-4">
+          <LabeledBtn
+            :cta="true"
+            label="Offer to help"
+            linkTo="https://tally.so/r/nprxbq"
+            fontSize="sm"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 defineProps<{
+  selected?: boolean;
+  event?: {
+    description?: string
+    plans?: Array<string>
+    disclaimer?: string
+  }
   feeds?: Array<{
     title?: string;
     description?: string;
