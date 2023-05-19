6cb9225e6332d0b0b48805fc4277d8ad40b84fec
nikhilagastya <karthikagastya2002@gmail.com>
2023-05-19 23:52:19 +0530

    Udated navigation

--- frontend/components/Carousel/Carousel.vue
+++ frontend/components/Carousel/Carousel.vue
@@ -1,80 +1,69 @@
 <template>
-  
-    <swiper :slides-per-view="1" :navigation="false" :pagination="true">
-      <swiper-slide v-for="n in 7" :key="n"> {{ n }} </swiper-slide>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-      
-    </swiper>
-  </template>
-  
-  <style scoped>
+  <swiper
+    class="swiper"
+    :modules="modules"
+    :slides-per-view="1"
+    :space-between="30"
+    :loop="true"
+    :pagination="{ clickable: true }"
+    :navigation="true"
+  >
+  <swiper-slide v-for="n in 7" :key="n"> {{ n }} </swiper-slide>
+  </swiper>
+</template>
 
-  .swiper-button-prev{
-    position: absolute;
-  top: 50%;
-  left: 27%;
-}
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Pagination, Navigation } from 'swiper'
+  import { Swiper, SwiperSlide } from 'swiper/vue'
+  import 'swiper/css'
+  import 'swiper/css/pagination'
+  import 'swiper/css/navigation'
 
-.swiper-button-next {
-  position: absolute;
-  top: 50%;
-  right: 27%;
-}
-.swiper-container {
-
-  position: relative;
-}
-
-  .swiper-slide {
-    display: flex;
-    height: 300px;
-    justify-content: center;
-    align-items: center;
-    border-radius: 20px;
-    width: 50% !important;
-    margin: 0 25%;
-    font-size: 24px;
-    font-weight: 700;
-  }
-  .swiper-slide:nth-child(1n) {
-    background-color: palevioletred;
-  }
-  .swiper-slide:nth-child(2n) {
-    background-color: skyblue;
-  }
-  .swiper-slide:nth-child(3n) {
-    background-color: peru;
-  }
-  .swiper-slide:nth-child(4n) {
-    background-color: cadetblue;
-  }
-  .swiper-slide:nth-child(5n) {
-    background-color: plum;
-  }
-  .swiper-slide:nth-child(6n) {
-    background-color: goldenrod;
-  }
-  .swiper-slide:nth-child(7n) {
-    background-color: palegreen;
-  }
-  </style>
-  <script>
-  // Import Swiper Vue.js components
-  import { Swiper } from "swiper/vue";
-  import { SwiperSlide } from "swiper/vue";
-  import SwiperCore, { Navigation, Pagination } from "swiper";
-  // Import Swiper styles
-  import "swiper/css";
-  import "swiper/css/navigation";
-  import "swiper/css/pagination";
-
-  navigation:{}
-  SwiperCore.use([Navigation , Pagination]);
-  export default {
+  export default defineComponent({
+    name: 'swiper-example-loop',
+    title: 'Loop mode / Infinite loop',
+    url: import.meta.url,
     components: {
       Swiper,
-      SwiperSlide,
+      SwiperSlide
     },
-  };
-  </script>
+    setup() {
+      return {
+        modules: [Pagination, Navigation]
+      }
+    }
+  })
+</script>
+
+<style scoped>
+.swiper-slide {
+  display: flex;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  font-weight: 700;
+}
+.swiper-slide:nth-child(1n) {
+  background-color: palevioletred;
+}
+.swiper-slide:nth-child(2n) {
+  background-color: skyblue;
+}
+.swiper-slide:nth-child(3n) {
+  background-color: peru;
+}
+.swiper-slide:nth-child(4n) {
+  background-color: cadetblue;
+}
+.swiper-slide:nth-child(5n) {
+  background-color: plum;
+}
+.swiper-slide:nth-child(6n) {
+  background-color: goldenrod;
+}
+.swiper-slide:nth-child(7n) {
+  background-color: palegreen;
+}
+</style>
