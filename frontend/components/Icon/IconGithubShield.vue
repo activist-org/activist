11612a41da9e71941b967bc5569bfc438bdb74b4
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-06-08 01:06:04 +0200

    #170 sizing for landing page GitHub shields

--- frontend/components/Icon/IconGithubShield.vue
+++ frontend/components/Icon/IconGithubShield.vue
@@ -1,10 +1,10 @@
 <template>
   <a
     :href="href"
-    class="flex items-center px-3 py-2 border space-x-2 card-style focus-brand text-light-text dark:text-dark-text border-light-text dark:border-dark-text bg-light-content dark:bg-dark-content hover:bg-light-highlight dark:hover:bg-dark-highlight"
+    class="flex items-center px-3 py-1 space-x-2 border card-style focus-brand text-light-text dark:text-dark-text border-light-text dark:border-dark-text bg-light-content dark:bg-dark-content hover:bg-light-highlight dark:hover:bg-dark-highlight"
   >
     <slot></slot>
-    <div v-if="text" class="text-xl font-semibold md:text-2xl">
+    <div v-if="text" class="text-lg font-semibold md:text-xl">
       {{ text }}
     </div>
     <div
@@ -13,7 +13,7 @@
     ></div>
     <div
       v-if="count"
-      class="flex items-center justify-center w-10 h-10 p-1 text-xl font-semibold rounded-full md:text-2xl md:h-11 md:w-11 bg-light-highlight dark:bg-dark-highlight"
+      class="flex items-center justify-center p-1 text-lg font-semibold rounded-full w-9 h-9 md:text-xl md:h-10 md:w-10 bg-light-highlight dark:bg-dark-highlight"
     >
       {{ count }}
     </div>
