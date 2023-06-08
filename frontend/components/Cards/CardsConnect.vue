9cc640ca4b2d4a9ecce9cd9fe7ee34ceabed5f28
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-06-08 09:24:16 +0200

    #154 add transition to popover and put above button

--- frontend/components/Cards/CardsConnect.vue
+++ frontend/components/Cards/CardsConnect.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center gap-5">
       <h3 class="text-left responsive-h3 font-display">Connect</h3>
       <div
-        class="p-1 break-all rounded-lg cursor-pointer transition-all hover:text-light-highlight dark:transition-all dark:hover:text-dark-highlight"
+        class="p-1 break-all transition-all rounded-lg cursor-pointer hover:text-light-highlight dark:transition-all dark:hover:text-dark-highlight"
       >
         <Icon
           v-if="userIsAdmin && !editModeEnabled"
@@ -20,11 +20,11 @@
       </div>
     </div>
     <ul
-      class="flex flex-col items-start mt-3 gap-2 md:flex-row md:items-center md:gap-6"
+      class="flex flex-col items-start gap-2 mt-3 md:flex-row md:items-center md:gap-6"
     >
       <li v-for="link in socialLinksRef">
         <div
-          class="flex items-center break-all cursor-pointer gap-3 transition-all"
+          class="flex items-center gap-3 break-all transition-all cursor-pointer"
           :class="{
             'hover:text-social-email': link.includes('email'),
             'hover:text-social-mastodon': link.includes('mastodon'),
@@ -39,11 +39,7 @@
             name="bi:x-lg"
             size="1em"
           />
-          <Icon 
-            v-if="link.includes('email')" 
-            name="bi:envelope" 
-            size="1.2em"
-          />
+          <Icon v-if="link.includes('email')" name="bi:envelope" size="1.2em" />
           <Icon
             v-else-if="link.includes('mastodon')"
             name="bi:mastodon"
@@ -70,26 +66,41 @@
           </div>
         </div>
       </li>
-      <div v-bind:style="[editModeEnabled ? {'visibility': 'visible'} : {'visibility': 'hidden'}]">
+      <div
+        v-bind:style="[
+          editModeEnabled
+            ? { visibility: 'visible' }
+            : { visibility: 'hidden' },
+        ]"
+      >
         <!--TODO: Update this to use the LabeledBtn component once it supports prepended icons.-->
-        <Popover class="relative" v-slot="{close}">
-          <PopoverButton 
-            as="div" 
-            class="flex items-center px-4 py-1 font-semibold text-white break-all cursor-pointer select-none gap-1 rounded-md text-md bg-light-cta-orange hover:bg-light-cta-orange-light active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-light dark:active:bg-dark-cta-orange focus-brand">
-              <Icon name="bi:plus" size="1.5em" />
-              New account
+        <Popover class="relative" v-slot="{ close }">
+          <PopoverButton
+            as="div"
+            class="flex items-center gap-1 px-4 py-1 font-semibold text-white break-all rounded-md cursor-pointer select-none text-md bg-light-cta-orange hover:bg-light-cta-orange-light active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-light dark:active:bg-dark-cta-orange focus-brand"
+          >
+            <Icon name="bi:plus" size="1.5em" />
+            New account
           </PopoverButton>
-          <PopoverPanel class="absolute z-10">
-            <div class="px-3 py-2">
+          <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="translate-y-1 opacity-0"
+            enter-to-class="translate-y-0 opacity-100"
+            leave-active-class="transition duration-100 ease-in"
+            leave-from-class="translate-y-0 opacity-100"
+            leave-to-class="translate-y-1 opacity-0"
+          >
+            <PopoverPanel class="absolute bottom-0 mb-12">
               <PopupNewField
-                @on-cta-clicked="account => emit('on-new-account', account)"
+                @on-cta-clicked="(account) => emit('on-new-account', account)"
                 @on-close-clicked="onClose(close)"
                 :title="'Add Account'"
                 :field-name-prompt="'Name'"
-                :cta-btn-label="'Add'">
+                :cta-btn-label="'Add'"
+              >
               </PopupNewField>
-            </div>
-          </PopoverPanel>
+            </PopoverPanel>
+          </transition>
         </Popover>
       </div>
     </ul>
@@ -97,7 +108,7 @@
 </template>
 
 <script setup lang="ts">
-import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 
 const props = defineProps<{
   socialLinks: Array<string>;
