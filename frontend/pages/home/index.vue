7c63feedbca1e0d9b79c384e25fbdbd143e3cdad
anthonyshibitov <anthonyshibitov@gmail.com>
2023-06-18 11:57:40 -0400

    Move home index page to new path

---
+++ frontend/pages/home/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div
+    class="text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content"
+  >
+    <Head>
+      <Title>{{ title }}</Title>
+    </Head>
+    <h1
+      class="pt-6 font-bold transition-all duration-500 responsive-h1 text-light-text dark:text-dark-text"
+      :class="{
+        'pl-56': sidebar.collapsed == false || sidebar.collapsedSwitch == false,
+        'pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
+      }"
+    >
+      Welcome, username 👋✊
+    </h1>
+    <flex class="relative flex items-center py-6"
+      ><h2
+        class="transition-all duration-500 responsive-h4 text-light-special-text dark:text-dark-special-text"
+        :class="{
+          'pl-56':
+            sidebar.collapsed == false || sidebar.collapsedSwitch == false,
+          'pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
+        }"
+      >
+        Your topics at a glance
+      </h2>
+      <TopicMarker topic="My topics dropdown" class="absolute right-7" />
+    </flex>
+    <div
+      class="pb-4 pr-2 space-y-4 sm:pr-8"
+      :class="{
+        'pl-56': sidebar.collapsed == false || sidebar.collapsedSwitch == false,
+        'pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
+      }"
+    >
+      <GridHomeMetrics />
+      <CardSearchResult
+        searchResultType="organization"
+        :isPrivate="false"
+        :organization="organization"
+      />
+      <CardSearchResult
+        searchResultType="event"
+        :isPrivate="false"
+        :event="event"
+      />
+      <CardSearchResult
+        searchResultType="resource"
+        :isPrivate="false"
+        :resource="resource"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Event } from "~~/types/event";
+import { Organization } from "~~/types/organization";
+import { Resource } from "~~/types/resource";
+const title = ref("Home");
+
+definePageMeta({
+  layout: "sidebar",
+});
+const sidebar = useSidebar();
+
+const resource: Resource = {
+  name: "Test Resource",
+  organizer: "Testers LLC",
+  resourceURL: "www.test.com",
+  description: "Test resource :D",
+  topic: "Tools",
+  relatedLocation: "Berlin",
+  creationDate: new Date(),
+  stars: 5,
+}
+
+const organization: Organization = {
+  name: "tech from below",
+  location: "Berlin",
+  description: "This is the description of tech from below",
+  topic: "Technology and Privacy",
+  members: 3,
+  supporters: 30,
+  imageURL: "/images/tech-from-below.svg",
+};
+
+const event: Event = {
+  name: "Test Event",
+  tagline: "We love to test!",
+  organizer: "Testers LLC",
+  topic: "Testing and Designing",
+  description: "This is a test event for testers.",
+  getInvolvedDescription: "Wanna help test?",
+  onlineLocation: "Zoom Test Room",
+  date: new Date(),
+  supporters: 10,
+};
+</script>
