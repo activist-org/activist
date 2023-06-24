0964d96ab6ba28ad688d8e773ac687533a20e7c5
momanyisamuel <momanyisamuel48@gmail.com>
2023-06-24 17:25:48 +0300

    Organisation about component #129

--- frontend/components/Card/CardAbout.vue
+++ frontend/components/Card/CardAbout.vue
@@ -2,31 +2,60 @@
   <div
     class="relative flex flex-col items-center w-full h-min card-style sm:items-start"
   >
-    <div class="bg-slate-400 w-10 h-10 sm:w-16 sm:h-16 absolute right-0 m-3 rounded-md"><img src="" alt=""></div>
+    <div
+      class="bg-slate-400 w-10 h-10 sm:w-16 sm:h-16 absolute right-0 m-3 rounded-md"
+    >
+      <img src="" alt="" />
+    </div>
     <div class="px-4 py-3 sm:px-2 sm:py-2 w-full">
       <div class="flex justify-start items-center gap-3 mb-3">
         <h3 class="text-left responsive-h3 font-display">About</h3>
         <Icon name="bi:pencil-square" size="1.2em" />
       </div>
-      <TopicMarker topic="Housing Community" class="mb-3" />
-      <div class="flex items-center gap-3 mb-3">
-        <div class="bg-slate-400 w-6 h-6 rounded-md"><img src="#" alt=""></div>
-        <p class="font-semibold">Berliner Obdachlosenhilfe</p>
-      </div>
-      <div class="flex flex-col sm:flex-row sm:items-center gap-3 mb-3">
-        <div class="flex items-center gap-2">
-          <Icon name="bx:bxs-map" size="1.2em" />
-          <p>Multiple locations, Berlin</p>
+      <div v-if="aboutType === 'event'">
+        <TopicMarker topic="Housing Community" class="mb-3" />
+        <div class="flex items-center gap-3 mb-3">
+          <div class="bg-slate-400 w-6 h-6 rounded-md">
+            <img src="#" alt="" />
+          </div>
+          <p class="font-semibold">Berliner Obdachlosenhilfe</p>
         </div>
-        <div class="flex items-center gap-3">
-          <Icon name="bi:calendar-plus" size="1.2em" />
-          <p>27/10/2022,14:00-23:30</p>
-          <Icon name="mdi:account-reactivate" size="1.2em" />
+        <div class="flex flex-col sm:flex-row sm:items-center gap-3 mb-3">
+          <div class="flex items-center gap-2">
+            <Icon name="bx:bxs-map" size="1.2em" />
+            <p>Multiple locations, Berlin</p>
+          </div>
+          <div class="flex items-center gap-3">
+            <Icon name="bi:calendar-plus" size="1.2em" />
+            <p>27/10/2022,14:00-23:30</p>
+            <Icon name="mdi:account-reactivate" size="1.2em" />
+          </div>
+        </div>
+        <div>
+          <p class="line-clamp-1 sm:line-clamp-none">{{ description }}</p>
+          <NuxtLink to="/" class="font-semibold text-blue-600"
+            >Click for full text</NuxtLink
+          >
         </div>
       </div>
-      <div>
-        <p>{{ description }}</p>
-        <NuxtLink to="/" class="font-semibold text-blue-600">Click for full text</NuxtLink>
+      <div v-if="aboutType === 'organization'">
+        <div class="flex flex-col sm:items-center gap-3 sm:flex-row mb-3">
+          <TopicMarker topic="Housing Community" class="" />
+          <div class="flex items-center gap-2">
+            <Icon name="bx:bxs-map" size="1.2em" />
+            <p>Berlin, Germany</p>
+          </div>
+          <div class="flex items-center gap-2">
+            <Icon name="bi:people" size="1.2em" />
+            <p>123 members</p>
+          </div>
+        </div>
+        <div>
+          <p class="line-clamp-1 sm:line-clamp-none">{{ description }}</p>
+          <NuxtLink to="/" class="font-semibold text-blue-600 sm:hidden"
+            >Click for full text</NuxtLink
+          >
+        </div>
       </div>
     </div>
   </div>
