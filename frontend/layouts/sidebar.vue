61add40601938f6e3b05782c4d974534a8f42a51
Mradosi <monika.radosiewicz@gmail.com>
2023-06-26 14:25:27 +0200

    change sidebar width (according to the designs), set layout sizes in layout file instead of on pages

--- frontend/layouts/sidebar.vue
+++ frontend/layouts/sidebar.vue
@@ -2,14 +2,18 @@
   <div class="flex h-screen overflow-hidden">
     <Sidebar />
     <div class="flex flex-col w-full overflow-y-scroll">
-      <slot />
-      <WebsiteFooter
-        :class="{
-          'pl-56':
-            sidebar.collapsed == false || sidebar.collapsedSwitch == false,
-          'pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
-        }"
-      />
+      <div class="bg-light-content dark:bg-dark-content" :class="{
+        'pl-2 sm:pl-24 md:pl-72':
+          sidebar.collapsed == false || sidebar.collapsedSwitch == false,
+        'pl-2 sm:pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
+      }" >
+        <slot />
+      </div>
+      <WebsiteFooter :class="{
+        'md:pl-72':
+          sidebar.collapsed == false || sidebar.collapsedSwitch == false,
+        'md:pl-24': sidebar.collapsed == true && sidebar.collapsedSwitch == true,
+      }" />
     </div>
     <MenuMobileNavBar />
   </div>
