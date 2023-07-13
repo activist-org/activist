77461914dec809201a4e786e312824c8e50fab07
Mradosi <monika.radosiewicz@gmail.com>
2023-07-13 13:16:00 +0200

    create sidebar filters

---
+++ frontend/components/Sidebar/SidebarFilters.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="px-3 py-8 text-light-text dark:text-dark-text opacity-0 transition"
+    :class="{ '!opacity-100': sidebar.collapsed == false || sidebar.collapsedSwitch == false }">
+    <template v-for="(filter, key) in filters">
+      <div class="flex items-center">
+        <h3 class="mb-3 font-bold responsive-h4" v-if="filter.title"> {{ filter.title }}</h3>
+        <div v-if="filter.slide">
+          <Icon name="bi:chevron-down" @click="filter.slideUp = !filter.slideUp" size="1em"
+            class="cursor-pointer flex-shrink-0  my-1 ml-4 mb-3" :class="{ 'rotate-180': filter.slideUp }" />
+        </div>
+      </div>
+      <div>
+        <FormRadioGroup v-if="filter.type === 'radio'" :key="key" :options="filter.items" :name="filter.name"
+          :title="filter.title" :style="filter.style" :allowCustomValue="filter.allowCustomValue"
+          v-model="selectedValues[filter.name]" class="mb-6" />
+        <keep-alive>
+          <FormCheckboxGroup v-if="filter.type === 'checkbox' && !filter.slideUp || filter.slideUp === false" :key="key"
+            :options="filter.items" :name="filter.name" :title="filter.title" :style="filter.style"
+            v-model="selectedValues[filter.name]" class="mb-1" />
+        </keep-alive>
+        <FormSearch v-if="filter.type === 'search'" :key="key" :name="filter.name" v-model="selectedValues[filter.name]"
+          :placeholder="filter.placeholder" class="mb-6" />
+      </div>
+    </template>
+  </div>
+</template>
+  
+<script setup lang="ts">
+const sidebar = useSidebar();
+import { ref, watch, Ref } from 'vue';
+
+interface FilterOption {
+  label: string;
+  value: string;
+}
+
+interface Filter {
+  title: string;
+  name: string;
+  type: 'radio' | 'checkbox' | 'search';
+  items: FilterOption[];
+  style?: string;
+  allowCustomValue?: boolean;
+  pageType?: string[];
+  searchInput?: boolean;
+  placeholder?: string;
+  slide?: boolean;
+  slideUp?: boolean;
+}
+
+interface Filters {
+  [key: string]: Filter;
+}
+
+interface SelectedValues {
+  [key: string]: string | string[];
+}
+
+const props = defineProps({
+  filters: {
+    type: Object as () => Filters,
+    required: true,
+  },
+});
+
+const filters = ref(props.filters);
+
+const selectedValues: Ref<SelectedValues> = ref({});
+
+watch(selectedValues.value, (newVal) => {
+  console.log('selectedValues changed');
+  console.log(newVal);
+
+  // TODO filters items based on selected filters
+});
+
+</script>
+
