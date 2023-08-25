ee073517f22834100bfa3e028203952768e5bd9f
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-25 19:02:44 -0300

    Linking /blog pages to i18n

--- frontend/pages/blog/index.vue
+++ frontend/pages/blog/index.vue
@@ -3,12 +3,11 @@
     class="text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content"
   >
     <Head>
-      <Title>{{ title }}</Title>
+      <Title>{{ $t("pages.blog.title") }}</Title>
     </Head>
-    <h1 class="p-4">This will be the home page of a blog</h1>
+    <h1 class="p-4">{{ $t("pages.blog.description") }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const title = ref("Blog Home");
 </script>
