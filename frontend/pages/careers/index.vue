0ea759165f57b1bd0f9f5c4184f3cf36cc9c4f96
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-25 19:04:59 -0300

    Linking /careers pages to i18n

--- frontend/pages/careers/index.vue
+++ frontend/pages/careers/index.vue
@@ -3,9 +3,9 @@
     class="text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content"
   >
     <Head>
-      <Title>{{ title }}</Title>
+      <Title>{{ $t("pages.careers.title") }}</Title>
     </Head>
-    <h1 class="p-4">This will be a careers landing page</h1>
+    <h1 class="p-4">{{ $t("pages.careers.description") }}</h1>
   </div>
 </template>
 
