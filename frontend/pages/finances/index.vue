ffb204ea3da7fc6c0249b06132a21d958d9edf1d
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-25 19:39:13 -0300

    Linking /finances pages to i18n

--- frontend/pages/finances/index.vue
+++ frontend/pages/finances/index.vue
@@ -3,12 +3,11 @@
     class="text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content"
   >
     <Head>
-      <Title>{{ title }}</Title>
+      <Title>{{ $t("pages.finances.title") }}</Title>
     </Head>
-    <h1 class="p-4">This will be a finances landing page</h1>
+    <h1 class="p-4">{{ $t("pages.finances.description") }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-const title = ref("Finances");
 </script>
