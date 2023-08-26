cab962fc48ff7917088a8ae1ff7ad5f3c5a7fcd3
otaree <deep.yen.ur.hash@gmail.com>
2023-08-26 22:56:40 +0530

    Update custom icon component to standard icon component

--- frontend/components/media/MediaVideo.vue
+++ frontend/components/media/MediaVideo.vue
@@ -1,27 +1,14 @@
 <template>
-  <!-- Note: mobile view -->
   <div
-    class="md:hidden w-full h-full card-style bg-light-placeholder dark:bg-dark-placeholder flex justify-center items-center"
+    class="w-full h-full card-style bg-light-placeholder dark:bg-dark-placeholder flex justify-center items-center"
   >
     <div
-      class="p-6 bg-light-special-text dark:bg-dark-menu-selection rounded-full"
+      class="p-6 md:p-8 bg-light-special-text dark:bg-dark-menu-selection rounded-full"
     >
-      <IconCameraVideo
-        width="75px"
-        height="75px"
-        :fill="$colorMode.preference === 'dark' ? 'white' : 'black'"
-      />
-    </div>
-  </div>
-  <!-- Note: desktop view -->
-  <div
-    class="hidden md:flex w-full h-full card-style bg-light-placeholder dark:bg-dark-placeholder justify-center items-center"
-  >
-    <div
-      class="p-8 bg-light-special-text dark:bg-dark-menu-selection rounded-full"
-    >
-      <IconCameraVideo
-        :fill="$colorMode.preference === 'dark' ? 'white' : 'black'"
+      <Icon
+        name="bi:camera-video"
+        :color="$colorMode.preference === 'dark' ? 'white' : 'black'"
+        size="75px"
       />
     </div>
   </div>
