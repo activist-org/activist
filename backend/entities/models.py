39733aa60cb59aaf38832eff86d9561ff743031c
kurtisolo <kurtisoloadc@gmail.com>
2023-08-31 17:09:17 +0200

    all models fixed

--- backend/entities/models.py
+++ backend/entities/models.py
@@ -7,8 +7,7 @@ import uuid
 Considerations:
 - All fields have on_delete=models.CASCADE: this needs to be reviewed, as SET_NULL is preferable in many cases.
 - More comments should be added to improve the readability and understanding of the code.
-- All BooleanFields need a default value
-- Some relational-models may need to be moved in the "events" app in order to prevent circular dependency issues.
+- Some relational-models may need to be moved in the "events" or "contents" app in order to prevent circular dependency issues.
 - In some/most cases a "ManyToManyField" may be more suitable than "ArrayField"
 """
 
@@ -49,15 +48,15 @@ class OrganizationApplication(models.Model):
 
 class OrganizationEvent(models.Model):
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    #event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event_id = models.ForeignKey('events.Event', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
 
-
+#a more souitable name for this model could be OrganizationMembership
 class OrganizationMember(models.Model):
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     is_owner = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_comms = models.BooleanField(default=False)
@@ -68,7 +67,7 @@ class OrganizationMember(models.Model):
 
 class OrganizationResource(models.Model):
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    #resource_id = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    resource_id = models.ForeignKey('contents.Resource', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -76,8 +75,8 @@ class OrganizationResource(models.Model):
 
 class OrganizationTask(models.Model):
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    #task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-    #group_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    task_id = models.ForeignKey('contents.Task', on_delete=models.CASCADE)
+    group_id = models.ForeignKey('contents.Task', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -85,11 +84,10 @@ class OrganizationTask(models.Model):
 
 class OrganizationTopic(models.Model):
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    #topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topic_id = models.ForeignKey('contents.Topic', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
-
 
 class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -97,9 +95,9 @@ class Group(models.Model):
     name = models.CharField(max_length=255)
     tagline = models.CharField(max_length=255)
     description = models.TextField(max_length=500)
-    #social_accounts = ArrayField(models.CharField(max_length=255))
+    social_accounts = ArrayField(models.CharField(max_length=255))
     total_flags = models.IntegerField(null=True)
-    #created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
     deletion_date = models.DateField(null=True)
 
@@ -109,7 +107,7 @@ class Group(models.Model):
 
 class GroupEvent(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    #event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event_id = models.ForeignKey('events.Event', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -117,7 +115,7 @@ class GroupEvent(models.Model):
 
 class GroupMember(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     is_admin = models.BooleanField(null=False)
 
     def __str__(self):
@@ -126,7 +124,7 @@ class GroupMember(models.Model):
 
 class GroupResource(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    #resource_id = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    resource_id = models.ForeignKey('contents.Resource', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -134,7 +132,7 @@ class GroupResource(models.Model):
 
 class GroupTopic(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
-    #topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topic_id = models.ForeignKey('contents.Topic', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
