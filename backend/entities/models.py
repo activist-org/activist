265ac390edc0ebd8082131eee9d6eb4b489e8b43
kurtisolo <139851573+kurtisolo@users.noreply.github.com>
2023-09-01 17:28:39 +0200

    Update models.py

--- backend/entities/models.py
+++ backend/entities/models.py
@@ -29,8 +29,6 @@ MODELS INDEX:
 
 
 
-
-
 class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
