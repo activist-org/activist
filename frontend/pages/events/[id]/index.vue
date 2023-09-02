cdcdf51f98472745e08ea231d13c50b8706d6b8a
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-09-02 17:30:37 -0300

    Adding the image

--- frontend/pages/events/[id]/index.vue
+++ frontend/pages/events/[id]/index.vue
@@ -1,11 +1,30 @@
 <template>
-	<div class="px-8 text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content">
-      <ImageEvent :eventType="teste" :imgURL="teste" />
+	<div class="px-8 py-8 text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content flex flex-col justify-between align-center">
+		<div
+			class="w-3/4 h-[260px] mx-auto"
+		>
+			<ImageEvent :eventType="event.type" :imgURL="event?.imageURL" />
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { Event } from "../../types/event";
+import { onMounted } from "vue";
 const { id } = useRoute().params;
+
+const event: Event = {
+  name: "Test Event",
+  type: "act",
+  tagline: "We love to test!",
+  organizer: "Testers LLC",
+  topic: "Testing and Designing",
+  description: "This is a test event for testers.",
+  getInvolvedDescription: "Wanna help test?",
+  onlineLocation: "Zoom Test Room",
+  date: new Date(),
+  supporters: 10,
+};
 
 
 onMounted(() => {
