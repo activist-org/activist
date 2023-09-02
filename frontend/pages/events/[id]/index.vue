f62d5748507d12700fb9065d268c0049e9f893d6
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-09-02 16:49:24 -0300

    Rediraction based on the width view

--- frontend/pages/events/[id]/index.vue
+++ frontend/pages/events/[id]/index.vue
@@ -1,7 +1,20 @@
-<template></template>
+<template>
+	<div class="px-8 text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content">
+      <ImageEvent :eventType="teste" :imgURL="teste" />
+	</div>
+</template>
 
 <script setup lang="ts">
 const { id } = useRoute().params;
 
-navigateTo(`${id}/about`);
+
+onMounted(() => {
+	redirectBasedOnScreenSize();
+});
+
+function redirectBasedOnScreenSize() {
+	if (window.innerWidth > 640) {
+		navigateTo(`/${id}/about`);
+	}
+}
 </script>
