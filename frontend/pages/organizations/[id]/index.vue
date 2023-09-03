a84e2bf73fec4449322f97a1f1266753c58c2c01
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-09-03 17:58:12 -0300

    Redirecting to mobile view on organizations page

--- frontend/pages/organizations/[id]/index.vue
+++ frontend/pages/organizations/[id]/index.vue
@@ -2,6 +2,28 @@
 
 <script setup lang="ts">
 const { id } = useRoute().params;
+import { Event } from "../../types/organization";
+import { onMounted } from "vue";
 
-navigateTo(`${id}/about`);
+const organization: Organization = {
+  name: "tech from below",
+  status: "approved",
+  tagline: "Technologie von und für soziale Bewegungen",
+  location: "Berlin",
+  description: "This is the description of tech from below",
+  topic: "Technology and Privacy",
+  members: 3,
+  supporters: 30,
+  imageURL: "/images/tech-from-below.svg",
+};
+
+onMounted(() => {
+  redirectBasedOnScreenSize();
+});
+
+function redirectBasedOnScreenSize() {
+  if (window.innerWidth > 384) {
+    navigateTo(`${id}/about`);
+  }
+}
 </script>
