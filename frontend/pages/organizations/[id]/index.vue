b812b96d66c6f757bf12ebb75af7eb851587abbd
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-09-05 14:22:04 -0300

    Adding buttons on organizations/[id] page

--- frontend/pages/organizations/[id]/index.vue
+++ frontend/pages/organizations/[id]/index.vue
@@ -23,12 +23,31 @@
         {{ organization.tagline }}
       </h2>
     </div>
+
+    <div 
+      class="w-full flex flex-col items-center gap-4"
+    >
+      <MenuLinkWrapper v-for="button in organizationButtons" :to="button.routeURL" :active="button.active" :selected="button.selected">
+        <div
+          class="flex items-center w-full text-sm font-medium text-left space-x-2"
+        >
+          <span class="width-1/6"
+            ><Icon v-if="button.iconURL" :name="button.iconURL" class="flex-shrink-0 w-5 h-5"
+          /></span>
+            <p
+              class="select-none width-5/6 whitespace-nowrap hover:light-menu-selection font-bold text-lg"
+            >
+              {{ $t(button.label) }}
+            </p>
+        </div>
+      </MenuLinkWrapper>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { id } = useRoute().params;
-import { Event } from "../../types/organization";
+import { Organization } from "../../types/organization";
 import { onMounted } from "vue";
 
 const organization: Organization = {
@@ -42,6 +61,72 @@ const organization: Organization = {
   supporters: 30,
   imageURL: "/images/tech-from-below.svg",
 };
+
+const organizationButtons: SidebarLeftSelectorType[] = [
+  {
+    label: "components.sidebar-left-selector.label.about",
+    routeURL: "/organizations/" + id + "/about",
+    iconURL: "bi:card-text",
+    selected: useRoute().path.split("/").pop() === "about" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.events",
+    routeURL: "/organizations/" + id + "/events",
+    iconURL: "bi:calendar-check",
+    selected: useRoute().path.split("/").pop() === "events" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.groups",
+    routeURL: "/organizations/" + id + "/groups",
+    iconURL: "IconGroup",
+    selected: useRoute().path.split("/").pop() === "groups" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.resources",
+    routeURL: "/organizations/" + id + "/resources",
+    iconURL: "IconResource",
+    selected: useRoute().path.split("/").pop() === "resources" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.faq",
+    routeURL: "/organizations/" + id + "/faq",
+    iconURL: "IconFAQ",
+    selected: useRoute().path.split("/").pop() === "faq" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.settings",
+    routeURL: "/organizations/" + id + "/settings",
+    iconURL: "bi:gear",
+    selected: useRoute().path.split("/").pop() === "settings" ? true : true,
+    active: true,
+  },
+  {
+    label: "components.sidebar-left-selector.label.affiliates",
+    routeURL: "/organizations/" + id + "/affiliates",
+    iconURL: "IconSupport",
+    selected: useRoute().path.split("/").pop() === "affiliates" ? true : true,
+    active: false,
+  },
+  {
+    label: "components.sidebar-left-selector.label.tasks",
+    routeURL: "/organizations/" + id + "/tasks",
+    iconURL: "bi:check-square",
+    selected: useRoute().path.split("/").pop() === "tasks" ? true : true,
+    active: false,
+  },
+  {
+    label: "components.sidebar-left-selector.label.discussions",
+    routeURL: "/organizations/" + id + "/discussions",
+    iconURL: "octicon:comment-discussion-24",
+    selected: useRoute().path.split("/").pop() === "discussions" ? true : true,
+    active: false,
+  },
+];
 
 onMounted(() => {
   redirectBasedOnScreenSize();
