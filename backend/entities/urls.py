469cfb4fef12775305b3fe04ec67d0f9a18f6088
kurtisolo <kurtisoloadc@gmail.com>
2023-09-10 11:55:24 +0200

    all basic viewsets initialized

--- backend/entities/urls.py
+++ backend/entities/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrganizationViewSet
+from . import views
 
 router = DefaultRouter()
-router.register(r'organizations', OrganizationViewSet, basename='organization')
+router.register(r'organizations', views.OrganizationViewSet, basename='organization')
+router.register(r'organization_application_statuses', views.OrganizationApplicationStatusViewSet)
+router.register(r'organization_applications', views.OrganizationApplicationViewSet)
+router.register(r'organization_events', views.OrganizationEventViewSet)
+router.register(r'organization_members', views.OrganizationMemberViewSet)
+router.register(r'organization_resources', views.OrganizationResourceViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'organization_tasks', views.OrganizationTaskViewSet)
+router.register(r'organization_topics', views.OrganizationTopicViewSet)
+router.register(r'group_events', views.GroupEventViewSet)
+router.register(r'group_members', views.GroupMemberViewSet)
+router.register(r'group_resources', views.GroupResourceViewSet)
+router.register(r'group_topics', views.GroupTopicViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- backend/entities/views.py
+++ backend/entities/views.py
@@ -1,8 +1,38 @@
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
-from .models import Organization
-from .serializers import OrganizationSerializer
+
+from .models import (
+    Organization, 
+    OrganizationApplicationStatus, 
+    OrganizationApplication,
+    OrganizationEvent, 
+    OrganizationMember, 
+    OrganizationResource, 
+    Group,
+    OrganizationTask, 
+    OrganizationTopic, 
+    GroupEvent, 
+    GroupMember,
+    GroupResource, 
+    GroupTopic
+)
+
+from .serializers import (
+    OrganizationSerializer,
+    OrganizationApplicationStatusSerializer, 
+    OrganizationApplicationSerializer,
+    OrganizationEventSerializer, 
+    OrganizationMemberSerializer, 
+    OrganizationResourceSerializer, 
+    GroupSerializer,
+    OrganizationTaskSerializer, 
+    OrganizationTopicSerializer, 
+    GroupEventSerializer, 
+    GroupMemberSerializer,
+    GroupResourceSerializer, 
+    GroupTopicSerializer
+    )
 
 
 class OrganizationViewSet(viewsets.ModelViewSet):
@@ -33,4 +63,52 @@ class OrganizationViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class OrganizationApplicationStatusViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationApplicationStatus.objects.all()
+    serializer_class = OrganizationApplicationStatusSerializer
+
+class OrganizationApplicationViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationApplication.objects.all()
+    serializer_class = OrganizationApplicationSerializer
+
+class OrganizationEventViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationEvent.objects.all()
+    serializer_class = OrganizationEventSerializer
+
+class OrganizationMemberViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationMember.objects.all()
+    serializer_class = OrganizationMemberSerializer
+
+class OrganizationResourceViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationResource.objects.all()
+    serializer_class = OrganizationResourceSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class OrganizationTaskViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationTask.objects.all()
+    serializer_class = OrganizationTaskSerializer
+
+class OrganizationTopicViewSet(viewsets.ModelViewSet):
+    queryset = OrganizationTopic.objects.all()
+    serializer_class = OrganizationTopicSerializer
+
+class GroupEventViewSet(viewsets.ModelViewSet):
+    queryset = GroupEvent.objects.all()
+    serializer_class = GroupEventSerializer
+
+class GroupMemberViewSet(viewsets.ModelViewSet):
+    queryset = GroupMember.objects.all()
+    serializer_class = GroupMemberSerializer
+
+class GroupResourceViewSet(viewsets.ModelViewSet):
+    queryset = GroupResource.objects.all()
+    serializer_class = GroupResourceSerializer
+
+class GroupTopicViewSet(viewsets.ModelViewSet):
+    queryset = GroupTopic.objects.all()
+    serializer_class = GroupTopicSerializer
 
