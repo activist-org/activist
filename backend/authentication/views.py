ad6ddb48ed90b18e716aa77faea01228061a5b0f
kurtisolo <kurtisoloadc@gmail.com>
2023-09-10 11:12:19 +0200

    added viewsets + updated urls

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import SupportEntityType, Support, User, UserResource, UserTask, UserTopic
+from .serializers import (
+    SupportEntityTypeSerializer, 
+    SupportSerializer, 
+    UserSerializer, 
+    UserResourceSerializer, 
+    UserTaskSerializer, 
+    UserTopicSerializer
+    )
 
-# Create your views here.
+
+class SupportEntityTypeViewSet(viewsets.ModelViewSet):
+    queryset = SupportEntityType.objects.all()
+    serializer_class = SupportEntityTypeSerializer
+
+class SupportViewSet(viewsets.ModelViewSet):
+    queryset = Support.objects.all()
+    serializer_class = SupportSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserResourceViewSet(viewsets.ModelViewSet):
+    queryset = UserResource.objects.all()
+    serializer_class = UserResourceSerializer
+
+class UserTaskViewSet(viewsets.ModelViewSet):
+    queryset = UserTask.objects.all()
+    serializer_class = UserTaskSerializer
+
+class UserTopicViewSet(viewsets.ModelViewSet):
+    queryset = UserTopic.objects.all()
+    serializer_class = UserTopicSerializer
---
+++ backend/content/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+router = DefaultRouter()
+router.register(r'resources', views.ResourceViewSet)
+router.register(r'tasks', views.TaskViewSet)
+router.register(r'topics', views.TopicViewSet)
+router.register(r'resource_topics', views.ResourceTopicViewSet)
+router.register(r'topic_formats', views.TopicFormatViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
