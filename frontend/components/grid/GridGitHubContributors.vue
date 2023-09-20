1632ce710ded7664e46c543303941589cbea1bd1
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-09-20 23:21:43 +0200

    #231 add in L10N strings and code formatting

--- frontend/components/grid/GridGitHubContributors.vue
+++ frontend/components/grid/GridGitHubContributors.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <ul class="flex justify-center md:justify-normal flex-wrap gap-4">
-      <li
-        v-for="item in githubData"
-        :key="item.loginID"
-      >
+      <li v-for="item in githubData" :key="item.loginID">
         <NuxtLink
           class="flex flex-col w-20 items-center focus-brand"
           :to="item.htmlUrl"
@@ -14,7 +11,9 @@
             :src="item.avatarUrl"
             alt=""
           />
-          <p class="truncate w-full text-center text-base text-light-text hover:text-light-text dark:text-dark-text dark:hover:text-dark-text">
+          <p
+            class="truncate w-full text-center text-base text-light-text hover:text-light-text dark:text-dark-text dark:hover:text-dark-text"
+          >
             {{ item.loginID }}
           </p>
         </NuxtLink>
@@ -23,16 +22,24 @@
         <button
           @click="onClickLoadMoreContributors"
           class="flex flex-col w-20 items-center focus-brand"
-          :aria-label="$t('components.grid-github-contributors.load-more')"
+          :aria-label="
+            $t('components.grid-github-contributors.load-more-aria-label')
+          "
         >
           <span
             class="rounded-full w-16 h-16 ring-2 hover:ring-light-cta-orange hover:ring-2 hover:ring-offset-2 ring-light-section-div dark:ring-dark-section-div dark:hover:ring-light-cta-orange dark:hover:ring-offset-dark-content bg-light-section-div dark:bg-dark-section-div flex items-center justify-center"
             alt=""
           >
-            <Icon name="bx:dots-horizontal-rounded" size="3em" class="text-light-text dark:text-dark-text" />
+            <Icon
+              name="bx:dots-horizontal-rounded"
+              size="3em"
+              class="text-light-text dark:text-dark-text"
+            />
           </span>
-          <p class="truncate w-full text-center text-base text-light-text hover:text-light-text dark:text-dark-text dark:hover:text-dark-text">
-            Load more
+          <p
+            class="truncate w-full text-center text-base text-light-text hover:text-light-text dark:text-dark-text dark:hover:text-dark-text"
+          >
+            {{ $t("components.grid-github-contributors.load-more-aria-label") }}
           </p>
         </button>
       </li>
@@ -58,7 +65,7 @@ onMounted(() => {
   fetchDataFromGitHubAPI(currentPage.value);
 });
 
-async function fetchDataFromGitHubAPI(page : number, numPerPage: number = 30) {
+async function fetchDataFromGitHubAPI(page: number, numPerPage: number = 30) {
   isLoading.value = true;
   try {
     const response = await fetch(
@@ -70,13 +77,15 @@ async function fetchDataFromGitHubAPI(page : number, numPerPage: number = 30) {
       hasMoreContributors.value = false;
     }
 
-    githubData.value = githubData.value.concat(data.map((item : any) => {
-      return {
-        avatarUrl: item.avatar_url,
-        htmlUrl: item.html_url,
-        loginID: item.login,
-      }
-    }));
+    githubData.value = githubData.value.concat(
+      data.map((item: any) => {
+        return {
+          avatarUrl: item.avatar_url,
+          htmlUrl: item.html_url,
+          loginID: item.login,
+        };
+      })
+    );
   } catch (error) {
     console.error(error);
   } finally {
@@ -85,7 +94,7 @@ async function fetchDataFromGitHubAPI(page : number, numPerPage: number = 30) {
 }
 
 function onClickLoadMoreContributors() {
-  currentPage.value ++;
+  currentPage.value++;
   fetchDataFromGitHubAPI(currentPage.value);
 }
 </script>
