d86d626c1f877abc0a98cfa425ef7f9f47db391e
sayi <sayikrishnapk@gmail.com>
2023-10-04 18:47:02 +0530

    #398 Add selector top bar

--- frontend/pages/events/[id]/settings.vue
+++ frontend/pages/events/[id]/settings.vue
@@ -1,4 +1,5 @@
 <template>
+  <SelectorTopBar :selectors="['Allies', 'Supporter', 'Supporting']"/>
   <div
     class="flex flex-col px-4 xl:px-8 text-light-text dark:text-dark-text bg-light-content dark:bg-dark-content"
   >
