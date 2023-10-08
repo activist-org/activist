81e9ce42b7c890eb3a53844460b277d133f9978b
hoon610 <123856266+hoon610@users.noreply.github.com>
2023-10-08 15:15:46 -0400

    Add BaseTooltip component and update it to the search bar

---
+++ frontend/components/tooltip/BaseTooltip.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col responsive-px-2 responsive-py-1 rounded shadow-md shadow-zinc-700 bg-light-distinct dark:bg-dark-distinct w-max text-light-content dark:text-dark-content tooltip">
+        <slot name="header">
+          <div class="responsive-h4 text-light-text dark:text-dark-text" v-if="header">{{ header }}</div>
+        </slot>
+        <slot name="text">
+          <div class="responsive-h5 text-light-text dark:text-dark-text" v-if="text">{{ text }}</div>
+        </slot>
+        <slot name="button" class="block"></slot>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps } from 'vue';
+  
+  defineProps<{
+    header: string | null;
+    text: string | null;
+  }>();
+  </script>
+  
