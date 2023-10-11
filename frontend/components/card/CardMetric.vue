8cbb4f78e94639ce19d5ef1209c7dac4120676cb
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-10-11 02:50:06 +0200

    #169 date picker for home page with wip light styles

--- frontend/components/card/CardMetric.vue
+++ frontend/components/card/CardMetric.vue
@@ -1,12 +1,14 @@
 <template>
   <div
-    class="max-w-full px-4 py-6 text-center border-4 cursor-pointer select-none card-style grid place-content-center rounded-2xl min-width-fit min-height-fit max-h-40"
+    class="max-w-full px-4 py-6 text-center border-4 cursor-pointer select-none card-style grid place-content-center rounded-2xl min-width-fit min-height-fit"
     :class="[textColor, borderColor, backgroundColor]"
   >
-    <div class="pb-2 text-5xl font-bold tracking-tighter">
+    <div
+      class="pb-2 text-5xl sm:text-6xl md:text-5xl xl:text-6xl font-bold tracking-tighter"
+    >
       {{ number }}
     </div>
-    <div class="text-lg font-bold">
+    <div class="text-lg xl:text-xl font-bold">
       {{ text }}
     </div>
   </div>
