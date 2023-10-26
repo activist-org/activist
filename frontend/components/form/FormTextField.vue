880fb5efbdd7fa94c19c9e48c2568d4b388e199c
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-10-26 22:39:12 +0200

    #493 revert change to FormTextField blur/focus emit

--- frontend/components/form/FormTextField.vue
+++ frontend/components/form/FormTextField.vue
@@ -8,8 +8,8 @@
   >
     <input
       @input="updateValue"
-      @blur="emit('update:modelValue', 'blurred')"
-      @focus="emit('update:modelValue', 'focused')"
+      @blur="emit('blurred')"
+      @focus="emit('focused')"
       :id="uuid"
       class="w-full h-5 bg-transparent outline-none placeholder:text-light-special-text dark:placeholder:text-dark-special-text"
       :value="modelValue"
