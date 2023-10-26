7a08617b665107adbae1f60a5e8d4abcd3d5dcf7
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-10-26 17:06:12 +0200

    #498 formatting and ux rearrange for legal disclosure

--- frontend/components/card/CardLegalDisclaimer.vue
+++ frontend/components/card/CardLegalDisclaimer.vue
@@ -2,25 +2,27 @@
   <Disclosure
     v-slot="{ open }"
     as="div"
-    class="flex flex-col px-4 py-2 mb-1 border select-none gap-3 rounded-md border-light-section-div dark:border-dark-section-div bg-light-header dark:bg-dark-header active:bg-light-highlight dark:active:bg-dark-highlight hover:cursor-pointer shadow-sm shadow-zinc-700"
+    class="flex flex-col gap-3 rounded-md border-light-section-div dark:border-dark-section-div bg-light-header dark:bg-dark-header active:bg-light-highlight dark:active:bg-dark-highlight hover:cursor-pointer shadow-sm shadow-zinc-700 focus-brand border"
   >
-    <DisclosureButton class="flex items-center gap-3 dark:text-light-distinct">
-      <Icon name="IconLegal" class="fill-light-text dark:fill-dark-text" />
-      <p>{{ $t("components.card-legal-disclaimer.header") }}</p>
-      <Icon v-if="open" name="bi:chevron-up" />
-      <Icon v-else name="bi:chevron-down" />
-    </DisclosureButton>
-
-    <DisclosurePanel
-      class="border-t border-light-section-div dark:border-dark-section-div"
-    >
-      <p
-        class="pt-2 select-text text-light-special-text dark:text-dark-special-text"
+    <DisclosureButton class="focus-brand px-4 py-2 rounded-md">
+      <div
+        class="flex gap-3 items-center text-light-text dark:text-dark-text select-none"
       >
-        {{ disclaimer }}
-      </p>
-    </DisclosurePanel>
-
+        <Icon name="IconLegal" class="fill-light-text dark:fill-dark-text" />
+        <p>{{ $t("components.card-legal-disclaimer.header") }}</p>
+        <Icon v-if="open" name="bi:chevron-up" />
+        <Icon v-else name="bi:chevron-down" />
+      </div>
+      <DisclosurePanel
+        class="border-t focus-within:border-0 border-light-section-div dark:border-dark-section-div mt-2 py-2"
+      >
+        <p
+          class="text-left select-text text-light-special-text dark:text-dark-special-text"
+        >
+          {{ disclaimer }}
+        </p>
+      </DisclosurePanel>
+    </DisclosureButton>
   </Disclosure>
 </template>
 
