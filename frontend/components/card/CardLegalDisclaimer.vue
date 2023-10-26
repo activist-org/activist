8a6d01c245018379fe267d01520ecc971adbbdc6
bmartins <pro@bmartins.fr>
2023-10-26 12:50:03 +0200

    chore: refactor component with HeadlessUI

--- frontend/components/card/CardLegalDisclaimer.vue
+++ frontend/components/card/CardLegalDisclaimer.vue
@@ -1,16 +1,17 @@
 <template>
-  <div
-    @click="toggleExpand()"
-    class="flex flex-col px-4 py-2 border select-none gap-3 rounded-md border-light-section-div dark:border-dark-section-div bg-light-header dark:bg-dark-header active:bg-light-highlight dark:active:bg-dark-highlight hover:cursor-pointer shadow-sm shadow-zinc-700"
+  <Disclosure
+    v-slot="{ open }"
+    as="div"
+    class="flex flex-col px-4 py-2 mb-1 border select-none gap-3 rounded-md border-light-section-div dark:border-dark-section-div bg-light-header dark:bg-dark-header active:bg-light-highlight dark:active:bg-dark-highlight hover:cursor-pointer shadow-sm shadow-zinc-700"
   >
-    <div class="flex items-center gap-3">
+    <DisclosureButton class="flex items-center gap-3 dark:text-light-distinct">
       <Icon name="IconLegal" class="fill-light-text dark:fill-dark-text" />
       <p>{{ $t("components.card-legal-disclaimer.header") }}</p>
-      <Icon v-if="isOpen" name="bi:chevron-up" />
+      <Icon v-if="open" name="bi:chevron-up" />
       <Icon v-else name="bi:chevron-down" />
-    </div>
-    <div
-      v-show="isOpen"
+    </DisclosureButton>
+
+    <DisclosurePanel
       class="border-t border-light-section-div dark:border-dark-section-div"
     >
       <p
@@ -18,17 +19,15 @@
       >
         {{ disclaimer }}
       </p>
-    </div>
-  </div>
+    </DisclosurePanel>
+
+  </Disclosure>
 </template>
 
 <script setup lang="ts">
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+
 defineProps<{
   disclaimer: string;
 }>();
-
-const isOpen = ref(false);
-const toggleExpand = () => {
-  isOpen.value = !isOpen.value;
-};
 </script>
