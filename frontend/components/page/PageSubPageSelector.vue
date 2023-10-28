14232a1f80ae9e5c710f69c21c68dbbf4e4b7179
Nicholas Vecchio <88710714+nicholas-vecchio@users.noreply.github.com>
2023-10-28 19:32:17 +1100

    PageSubPageSelector changed to use HeadlessUI.
    - Changed to use Headless UI components
    
    - Manually handling pushes otherwise headlessui accessibility will not function.

--- frontend/components/page/PageSubPageSelector.vue
+++ frontend/components/page/PageSubPageSelector.vue
@@ -1,28 +1,52 @@
 <template>
   <div class="w-full h-fit">
-    <div class="flex flex-row">
-      <NuxtLink
-        v-for="s in selectors"
-        :key="s.id"
-        class="w-full justify-center flex px-3 py-1 border-l-[1px] border-y-[1px] border-light-text dark:border-dark-text"
-        :class="{
-          'bg-light-menu-selection dark:bg-dark-menu-selection text-light-distinct dark:text-dark-distinct hover:bg-light-menu-selection/90 dark:hover:bg-dark-menu-selection/90':
-            s.selected,
-          'bg-light-header dark:bg-dark-header text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight':
-            !s.selected,
-        }"
-        :to="localePath(s.routeURL)"
-      >
-        {{ s.label }}
-      </NuxtLink>
-    </div>
+    <TabGroup v-model="currentTabIndex"
+    manual
+    @change="changeTab"
+    :default-index="defaultIndex"
+    >
+      <TabList class="flex flex-row">
+        <Tab v-for="selector in selectors" :key="selector.id" class="w-full">
+          <template v-slot="{ selected }">
+            <NuxtLink
+              :class="[
+                'justify-center flex px-3 py-1 border-l-[1px] border-y-[1px] w-full',
+                'border-light-text dark:border-dark-text',
+                selected
+                  ? 'bg-light-menu-selection dark:bg-dark-menu-selection text-light-distinct dark:text-dark-distinct hover:bg-light-menu-selection/90 dark:hover:bg-dark-menu-selection/90'
+                  : 'bg-light-header dark:bg-dark-header text-light-text dark:text-dark-text hover:bg-light-highlight dark:hover:bg-dark-highlight',
+              ]"
+              :to="localePath(selector.routeURL)"
+            >
+              {{ selector.label }}
+            </NuxtLink>
+          </template> 
+        </Tab>
+      </TabList>
+    </TabGroup>
   </div>
 </template>
 
 <script setup lang="ts">
-import { SubPageSelector } from "~/types/sub-page-selector";
+import { computed } from 'vue';
+import { Tab, TabList, TabGroup } from '@headlessui/vue';
+import { SubPageSelector } from '~/types/sub-page-selector';
 
-defineProps<{
+const router = useRouter();
+const nuxtApp = useNuxtApp();
+
+const props = defineProps<{
   selectors: SubPageSelector[];
 }>();
+
+const defaultIndex = computed(() => {
+  return props.selectors.findIndex(selector => selector.selected) || 0;
+});
+
+function changeTab(index: number) {
+  const selectedRoute = props.selectors[index]?.routeURL;
+  if (selectedRoute) {
+    router.push(nuxtApp.$localePath(selectedRoute));
+  }
+}
 </script>
