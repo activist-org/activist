40b794421861e7a975319b67b3ae4c4145d49851
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-11-05 15:25:18 +0100

    #530 rename i18n value check workflow and expand script

--- frontend/i18n/find_repeat_i18n_values.py
+++ frontend/i18n/find_repeat_i18n_values.py
@@ -5,6 +5,7 @@ Run `python find_repeat_i18n_values.py` to check if the en-US.json file has repe
 If yes, combine them in the `_common` sub object at the lowest shared depth of `en-US.json`.
 """
 
+
 from collections import Counter
 import json
 from pathlib import Path
@@ -15,28 +16,49 @@ this_directory = str(Path(__file__).parent.resolve())
 with open(f"{this_directory}/en-US.json") as f:
     en_us_json_dict = json.loads(f.read())
 
+def lower_and_remove_punctuation(value):
+    return value.lower().translate(str.maketrans("", "", string.punctuation))
 
-def get_nested_dict_values(d):
-    for v in d.values():
-        if isinstance(v, dict):
-            yield from get_nested_dict_values(v)
-        else:
-            yield v
-
-
-all_json_keys = [
-    k.lower().translate(str.maketrans("", "", string.punctuation))
-    for k in list(get_nested_dict_values(en_us_json_dict))
+all_json_values = [
+    lower_and_remove_punctuation(value=v)
+    for v in list(en_us_json_dict.values())
 ]
 
-json_repeat_key_counts = {
-    k: v for k, v in dict(Counter(all_json_keys)).items() if v > 1
+json_repeat_value_counts = {
+    k: v for k, v in dict(Counter(all_json_values)).items() if v > 1
 }
 
-print(
-    json.dumps(
-        json_repeat_key_counts,
-        indent=2,
-    )
-)
-print(f"Number of repeat i18n values: {len(json_repeat_key_counts)}")
+for repeat_value in json_repeat_value_counts:
+    i18n_keys = [
+        k
+        for k, v in en_us_json_dict.items()
+        if repeat_value == lower_and_remove_punctuation(value=v)
+    ]
+
+    print(f"\nRepeat value: '{repeat_value}'")
+    print(f"Number of instances: : {json_repeat_value_counts[repeat_value]}")
+    print(f"Keys: {', '.join(list(i18n_keys))}")
+
+    common_prefix = ""
+    min_key_length = min(len(k) for k in i18n_keys)
+    common_character = True
+    while common_character:
+        for i in range(min_key_length):
+            if len({k[i] for k in i18n_keys}) == 1:
+                common_prefix += i18n_keys[0][i]
+            else:
+                common_character = False
+                break
+
+        common_character = False
+
+    if common_prefix := '.'.join(common_prefix.split('.')):
+        print(f"Suggested new key: {'.'.join(common_prefix.split('.'))[:-1]}._global.IDENTIFIER_KEY")
+    else:
+        print("Suggested new key: _global.IDENTIFIER_KEY")
+
+if json_repeat_value_counts:
+    print(f"\nNumber of repeat i18n values: {len(json_repeat_value_counts)}")
+
+else:
+    print("\nNo repeat i18n values found.")
