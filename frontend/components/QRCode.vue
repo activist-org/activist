009dcd31f974ea76f6e85fcea28c65282812094d
mjiruobe <68758914+mjiruobe@users.noreply.github.com>
2023-11-18 23:31:32 +0100

    added qr code in svg format; download in different

--- frontend/components/QRCode.vue
+++ frontend/components/QRCode.vue
@@ -1,24 +1,85 @@
 <template>
   <div id="qrcode" class="flex flex-col w-max">
-    <div
-      class="flex w-max justify-center relative border-2 border-black bg-white rounded-t-3xl"
-    >
-      <qrcode-vue class="p-4" :value="getPath()" :size="codeSize" />
-      <div
-        class="h-16 w-16 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full bg-white absolute fill-black"
+    <div class="flex w-max justify-center relative">
+      <svg
+        id="resultqr"
+        :width="`${codeSize + quietZone * 2 + 2}`"
+        :height="`${codeSize + quietZone * 2 + fontSize + textPaddingY * 2}`"
+        :viewBox="`${-quietZone} ${-quietZone - 1} ${
+          codeSize + quietZone * 2
+        } ${codeSize + quietZone * 2 + fontSize + textPaddingY * 2}`"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <IconActivist
-          class="cursor-none pointer-events-none flex items-center w-10 h-14 absolute -top-[0.1rem] m-auto left-[0.6rem] overflow-clip"
-          color="fill-black dark:fill-black hover:fill-black"
+        <rect
+          id="generaledges"
+          :rx="borderRadius"
+          :width="`${codeSize + quietZone * 2}`"
+          :height="`${codeSize + quietZone * 2 + fontSize + textPaddingY * 2}`"
+          fill="#FFFFFF"
+          :x="`${-quietZone}`"
+          :y="`${-quietZone}`"
+          style="stroke-width: 2; stroke: #000000"
         />
-      </div>
-    </div>
-    <div
-      class="flex justify-center py-2 bg-black rounded-b-3xl text-white w-full"
-    >
-      <p class="pb-1 text-2xl">
-        {{ $t("components.qr-code.url-text") }}
-      </p>
+        <qrcode-vue
+          ref="qrref"
+          class="qrimage"
+          :value="getPath()"
+          :size="codeSize"
+          render-as="svg"
+        />
+        <rect
+          id="removetopedgeofbottom"
+          :width="`${codeSize + quietZone * 2}`"
+          :height="`${borderRadius / 2}`"
+          fill="#000000"
+          :x="`${-quietZone}`"
+          :y="`${codeSize + quietZone * 2 - quietZone}`"
+          style="stroke-width: 1; stroke: #000000"
+        />
+        <rect
+          id="fillbottom"
+          :rx="borderRadius"
+          :width="`${codeSize + quietZone * 2}`"
+          :height="`${fontSize + textPaddingY * 2}`"
+          fill="#000000"
+          :x="`${-quietZone}`"
+          :y="`${codeSize + quietZone * 2 - quietZone}`"
+          style="stroke-width: 1; stroke: #000000"
+        />
+        <text
+          font-family="Red Hat Display"
+          :style="`font-size: ${fontSize}px;`"
+          :x="`${(codeSize + quietZone * 2) / 2 - quietZone}`"
+          :y="`${
+            codeSize + quietZone * 2 + fontSize + textPaddingY - quietZone
+          }`"
+          dominant-baseline="ideographic"
+          text-anchor="middle"
+          fill="#FFFFFF"
+        >
+          activist.org
+        </text>
+        <circle
+          :r="`${logoBGRadius * 0.5}px`"
+          :cx="`${(codeSize + quietZone * 2) / 2 - quietZone}`"
+          :cy="`${(codeSize + quietZone * 2) / 2 - quietZone}`"
+          fill="#FFFFFF"
+        />
+        <svg
+          :width="`${logoSize}`"
+          :height="`${logoSize}`"
+          :x="`${
+            (codeSize + quietZone * 2) / 2 - quietZone - logoSize / 2 - 2
+          }`"
+          :y="`${(codeSize + quietZone * 2) / 2 - quietZone - logoSize / 2}`"
+          viewBox="0 0 503 494.8"
+        >
+          <path
+            d="M 116.208 247.423 C 116.208 319.895 174.947 378.636 247.418 378.636 C 283.639 378.636 316.456 363.958 340.189 340.195 L 154.647 154.65 C 130.915 178.383 116.208 211.201 116.208 247.423 Z M 430.237 8.996 H 503.24 V 485.82 H 430.237 C 419.406 485.82 410.41 477.711 410.41 466.881 V 433.471 C 366.881 471.645 309.887 494.816 247.448 494.816 C 110.763 494.846 0 384.051 0 247.423 C 0 110.794 110.763 0 247.418 0 C 309.857 0 366.851 23.171 410.381 61.345 V 27.935 C 410.381 17.104 419.406 8.996 430.207 8.996 H 430.237 Z M 411.92 247.423 C 411.92 156.574 338.265 82.918 247.418 82.918 C 156.571 82.918 82.9166 156.574 82.9166 247.423 C 82.9166 338.272 156.571 411.927 247.418 411.927 C 338.265 411.927 411.92 338.272 411.92 247.423 Z"
+            fill="#000000"
+          />
+        </svg>
+      </svg>
     </div>
   </div>
 </template>
@@ -27,10 +88,31 @@
 import QrcodeVue from "qrcode.vue";
 import { ref } from "vue";
 
-const codeSize = ref(250);
+const fontSize = ref(24);
+const quietZone = ref(18);
+const textPaddingY = ref(14);
+const codeSize = ref(218);
+const logoBGRadius = ref(64);
+const logoSize = ref(40);
+const qrref = ref(null);
+
+const borderRadius = ref(36);
 
 function getPath() {
   const host = window.location.toString();
   return host;
 }
+
+function getSize() {
+  return {
+    width: codeSize.value + quietZone.value * 2 + 2,
+    height:
+      codeSize.value +
+      quietZone.value * 2 +
+      fontSize.value +
+      textPaddingY.value * 2,
+  };
+}
+
+defineExpose({ getSize });
 </script>
--- frontend/components/btn/BtnLabeled.vue
+++ frontend/components/btn/BtnLabeled.vue
@@ -77,51 +77,92 @@
       {{ counter }}
     </div>
   </a>
-  <NuxtLink
-    v-else
-    :to="localePath(`${linkTo}`)"
-    class="px-4 py-2 font-semibold text-center border select-none rounded-md xl:rounded-lg focus-brand w-fit shadow-sm shadow-zinc-700"
-    :class="{
-      'text-light-text border-light-text dark:text-dark-cta-orange dark:border-dark-cta-orange  fill-light-text dark:fill-dark-cta-orange bg-light-cta-orange dark:bg-dark-cta-orange/10 hover:bg-light-cta-orange-hover active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-hover/25 dark:active:bg-dark-cta-orange/10':
-        cta == true,
-      'text-light-text border-light-text dark:text-dark-text dark:border-dark-text bg-light-btn dark:bg-dark-btn hover:bg-light-highlight active:bg-light-btn dark:hover:bg-dark-highlight dark:active:bg-dark-btn ':
-        cta == false,
-      'text-xs': fontSize == 'xs',
-      'text-sm': fontSize == 'sm',
-      'text-base': fontSize == 'base',
-      'text-lg': fontSize == 'lg',
-      'text-base sm:text-lg xl:text-xl xl:px-6 xl:py-3': fontSize == 'xl',
-      'text-base sm:text-lg xl:text-2xl xl:px-6 xl:py-3': fontSize == '2xl',
-      'text-base sm:text-lg xl:text-3xl xl:px-6 xl:py-3': fontSize == '3xl',
-    }"
-    :aria-label="$t(ariaLabel)"
-  >
-    <div class="flex items-center">
-      <Icon
-        v-if="leftIcon"
-        class="mr-2 -my-1"
-        :name="leftIcon"
-        :size="iconSize"
-      />
-      <span v-if="label" class="mx-auto">{{ $t(label) }}</span>
-      <div v-else class="-mx-1"></div>
-      <Icon
-        v-if="rightIcon"
-        class="ml-2 -my-1"
-        :name="rightIcon"
-        :size="iconSize"
-      />
-      <div
-        v-if="counter"
-        class="px-1 py-[0.1rem] ml-2 -m-[0.1rem] rounded-lg bg-black/20 dark:bg-dark-cta-orange/25"
-      >
-        {{ counter }}
+  <div v-else>
+    <NuxtLink
+      :to="localePath(`${linkTo}`)"
+      class="flex items-center px-4 py-2 font-semibold text-center border select-none rounded-md xl:rounded-lg focus-brand w-fit shadow-sm shadow-zinc-700"
+      :class="{
+        'text-light-text border-light-text dark:text-dark-cta-orange dark:border-dark-cta-orange  fill-light-text dark:fill-dark-cta-orange bg-light-cta-orange dark:bg-dark-cta-orange/10 hover:bg-light-cta-orange-hover active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-hover/25 dark:active:bg-dark-cta-orange/10':
+          cta == true,
+        'text-light-text border-light-text dark:text-dark-text dark:border-dark-text bg-light-btn dark:bg-dark-btn hover:bg-light-highlight active:bg-light-btn dark:hover:bg-dark-highlight dark:active:bg-dark-btn ':
+          cta == false,
+        'text-xs': fontSize == 'xs',
+        'text-sm': fontSize == 'sm',
+        'text-base': fontSize == 'base',
+        'text-lg': fontSize == 'lg',
+        'text-base sm:text-lg xl:text-xl xl:px-6 xl:py-3': fontSize == 'xl',
+        'text-base sm:text-lg xl:text-2xl xl:px-6 xl:py-3': fontSize == '2xl',
+        'text-base sm:text-lg xl:text-3xl xl:px-6 xl:py-3': fontSize == '3xl',
+      }"
+      :aria-label="$t(ariaLabel)"
+    >
+      <div class="flex items-center">
+        <Icon
+          v-if="leftIcon"
+          class="mr-2 -my-1"
+          :name="leftIcon"
+          :size="iconSize"
+        />
+        <span v-if="label" class="mx-auto">{{ $t(label) }}</span>
+        <div v-else class="-mx-1"></div>
+        <Icon
+          v-if="rightIcon"
+          class="ml-2 -my-1"
+          :name="rightIcon"
+          :size="iconSize"
+        />
+        <div
+          v-if="counter"
+          class="px-1 py-[0.1rem] ml-2 -m-[0.1rem] rounded-lg bg-black/20 dark:bg-dark-cta-orange/25"
+        >
+          {{ counter }}
+        </div>
       </div>
+    </NuxtLink>
+    <div v-if="optionsCallback" @click.stop class="relative">
+      <Menu>
+        <MenuButton
+          class="flex shadow-zinc-700 items-center px-4 py-2 font-semibold text-center border select-none rounded-r-md xl:rounded-r-lg focus-brand w-fit shadow-sm shadow-zinc-70 text-light-text border-light-text dark:text-dark-cta-orange dark:border-dark-cta-orange fill-light-text dark:fill-dark-cta-orange bg-light-cta-orange dark:bg-dark-cta-orange/10 hover:bg-light-cta-orange-hover active:bg-light-cta-orange dark:hover:bg-dark-cta-orange-hover/25 dark:active:bg-dark-cta-orange/10"
+          :class="{
+            'text-xs': fontSize == 'xs',
+            'text-sm': fontSize == 'sm',
+            'text-base': fontSize == 'base',
+            'text-lg': fontSize == 'lg',
+            'text-base sm:text-lg xl:text-xl xl:px-6 xl:py-3': fontSize == 'xl',
+            'text-base sm:text-lg xl:text-2xl xl:px-6 xl:py-3':
+              fontSize == '2xl',
+            'text-base sm:text-lg xl:text-3xl xl:px-6 xl:py-3':
+              fontSize == '3xl',
+          }"
+          >&#x25BC;</MenuButton
+        >
+        <MenuItems
+          class="absolute right-0 md:right-auto md:fixed z-10 bg-white"
+        >
+          <MenuItem
+            v-for="option in options"
+            :key="option"
+            v-slot="{ active }"
+            class="cursor-pointer text-gray-700 block px-4 text-sm py-2"
+          >
+            <a
+              @click="optionsCallback(option)"
+              :class="{
+                'bg-light-cta-orange dark:bg-dark-cta-orange/10': active,
+              }"
+            >
+              {{ option }}
+            </a>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
     </div>
-  </NuxtLink>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
 export interface Props {
   cta: boolean;
   linkTo?: string;
@@ -132,6 +173,8 @@ export interface Props {
   iconSize?: string;
   counter?: number;
   ariaLabel: string;
+  options?: string[];
+  optionsCallback?: (option: string) => void;
 }
 
 withDefaults(defineProps<Props>(), {
