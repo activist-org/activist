c9b70836094097719bc9b324e70217384af7535c
PabloSSena <pablosilvasena@gmail.com>
2023-11-18 15:02:58 -0300

    Just starting the changes

--- frontend/components/media/MediaDatePicker.vue
+++ frontend/components/media/MediaDatePicker.vue
@@ -3,31 +3,59 @@
     v-if="$colorMode.preference == 'light'"
     class="w-full h-full rounded-[0.45em] shadow-sm shadow-zinc-700"
   >
-    <DatePicker
+    <!-- <DatePicker
       v-model="date"
       :first-day-of-week="2"
       color="light-mode"
       trim-weeks
       expanded
-    />
+    /> -->
+    <VCalendar :attributes='attrs' />
+    <!-- <VDatePicker v-model="date" /> -->
+
   </form>
   <form v-else class="w-full h-full rounded-[0.45em] shadow-sm shadow-zinc-700">
-    <DatePicker
+    <VCalendar :attributes='attrs' />
+    <!-- <VDatePicker
       v-model="date"
       :first-day-of-week="2"
       color="dark-mode"
       :is-dark="true"
       trim-weeks
       expanded
-    />
+      :select-attribute="attrs"
+    /> -->
+    <!-- <VCalendar :is-dark="isDark" /> -->
+    <!-- <VDatePicker v-model="date" /> -->
   </form>
 </template>
 
 <script setup>
 import { DatePicker } from "v-calendar";
+import VCalendar from 'v-calendar';
+import { ref } from 'vue';
 import "v-calendar/style.css";
-
+const attrs = ref([
+{
+        // An optional key can be used for retrieving this attribute later,
+        // and will most likely be derived from your data object
+        key: Any,
+        // Attribute type definitions
+        content: 'red',   // Boolean, String, Object
+        highlight: true,  // Boolean, String, Object
+        dot: true,        // Boolean, String, Object
+        bar: true,        // Boolean, String, Object
+        // Your custom data object for later access, if needed
+        // We also need some dates to know where to display the attribute
+        // We use a single date here, but it could also be an array of dates,
+        //  a date range or a complex date pattern.
+        dates: new Date(),
+        // Think of `order` like `z-index`
+        order: 0
+      }
+]);
 const date = ref(new Date());
+const markedDates = ref([new Date("2023-11-15"), new Date("2023-11-20")]); // Adicione as datas que deseja marcar aqui
 </script>
 
 <style>
