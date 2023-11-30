c8b69156c04c8000fab381a7d3e8ba2db83f2260
TeddyGavi <matc.davis@gmail.com>
2023-11-29 16:47:01 -0800

    refactor(card topic selection): to use headless ui tab component

--- frontend/components/card/CardTopicSelection.vue
+++ frontend/components/card/CardTopicSelection.vue
@@ -1,67 +1,59 @@
-<template>
-  <Combobox
-    v-model="value"
-    v-slot="{ open }"
-    multiple
-    as="div"
-    class="flex-col space-y-3 w-full card-style px-5 py-6"
-  >
-    <ComboboxLabel
-      as="h2"
+<template v-model="value">
+  <div class="flex-col space-y-3 w-full card-style px-5 py-6">
+    <label
+      for="query"
       class="block font-medium responsive-h3 dark:text-dark-text"
     >
       {{ $t("components.card-topic-selection.header") }}
-    </ComboboxLabel>
-
-    <ComboboxInput
-      @change="query = $event.target.value"
+    </label>
+    <input
+      v-model="query"
+      id="query"
       :display-value="() => query"
       :placeholder="$t('components.card-topic-selection.selector-placeholder')"
       class="pl-4 py-2 w-full text-light-special-text dark:text-dark-special-text bg-light-header dark:bg-dark-header rounded-md elem-shadow-sm"
     />
-
-    <ComboboxOptions
-      static
-      class="flex flex-col gap-2 md:flex-row md:items-center"
-    >
-      <ComboboxOption
-        v-for="topic of filteredTopics"
-        v-slot="{ active, selected }"
-        :key="topic.value"
-        :value="topic.value"
-        as="template"
-      >
-        <li
-          class="flex justify-between items-center gap-2 rounded-lg p-2 border bg-light-btn border-dark-btn font-bold cursor-pointer hover:bg-light-cta-orange-hover hover:dark:bg-dark-cta-orange-hover elem-shadow-sm"
-          :class="{
-            'outline outline-blue-400 bg-light-cta-orange-hover dark:bg-dark-cta-orange-hover':
-              active && open,
-            'bg-light-cta-orange dark:bg-dark-cta-orange': selected,
-          }"
+    <TabGroup manual class="flex flex-col gap-2 md:flex-row md:items-center">
+      <TabList>
+        <Tab
+          v-for="topic of filteredTopics"
+          @click="selectTopic(topic)"
+          @keydown.enter.prevent="selectTopic(topic)"
+          v-slot="{ selected }"
+          :key="topic.value"
+          :value="topic.value"
+          multiple
+          as="template"
+          :selectedTab="selectedTab"
+          class="flex justify-between focus:ring-4 items-center gap-2 rounded-lg p-2 border bg-light-btn border-dark-btn font-bold cursor-pointer hover:bg-light-cta-orange-hover hover:dark:bg-dark-cta-orange-hover shadow-sm shadow-zinc-700"
         >
-          <span class="flex items-center gap-2">
-            <Icon :name="topic.icon" size="20" />
-            {{ $t(topic.label) }}
-          </span>
-          <Icon
-            v-if="selected"
-            name="bi:x-lg"
-            class="cursor-pointer"
-            size="20"
-          />
-        </li>
-      </ComboboxOption>
-    </ComboboxOptions>
-  </Combobox>
+          <div
+            :class="{
+              'focus:outline focus:outline-blue-400': selected,
+              'bg-light-cta-orange dark:bg-dark-cta-orange': isActiveTopic(
+                topic.value
+              ),
+            }"
+          >
+            <span class="flex items-center gap-2">
+              <Icon :name="topic.icon" size="20" />
+              {{ $t(topic.label) }}
+            </span>
+            <Icon
+              v-if="isActiveTopic(topic.value)"
+              name="bi:x-lg"
+              class="cursor-pointer"
+              size="20"
+            />
+          </div>
+        </Tab>
+      </TabList>
+    </TabGroup>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxOption,
-  ComboboxOptions,
-} from "@headlessui/vue";
+import { TabGroup, TabList, Tab } from "@headlessui/vue";
 
 import { GLOBAL_TOPICS, Topic, TopicsTag } from "~/types/topics";
 
@@ -85,6 +77,19 @@ const value = computed<Topic[]>({
 });
 
 const query = ref("");
+const selectedTab = ref(0);
+
+const selectTopic = (topic: TopicsTag) => {
+  const updatedValue = [...props.modelValue];
+  const index = updatedValue.indexOf(topic.value);
+
+  if (index === -1) {
+    updatedValue.push(topic.value);
+  } else {
+    updatedValue.splice(index, 1);
+  }
+  value.value = updatedValue;
+};
 
 const topics = computed((): TopicsTag[] => {
   return [
