80e0fbc52eec7635dacd5d70f5900864688af59d
mahasultan <ajabor@andrew.cmu.edu>
2023-12-02 03:39:41 +0300

    Added Tooltip Feature and functionality to TooltipDiscussionWarning.vue

---
+++ frontend/components/tooltip/TooltipDiscussionWarning.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tooltip
+    :text="text"
+    class="w-80 lg:right-0"
+    >
+    <ul class="list-disc pl-4 space-y-1">
+      <li>Names</li>
+      <li>Home addresses</li>
+      <li>Phone numbers</li>
+      <li>Email addresses</li>
+    </ul>
+  </Tooltip>
+</template>
+
+<script setup lang="ts">
+import Tooltip from './tooltip.vue';
+
+const props = withDefaults(defineProps<{
+  text?: string;
+}>(), {
+  text: 'Please use usernames and do NOT write any personal information including:'
+});
+</script>
