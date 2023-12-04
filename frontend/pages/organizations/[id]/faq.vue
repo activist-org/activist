c395986aeac314781c5997413fb4e7e41745b2d7
Matt Burnett <mburnett@tutanota.de>
2023-12-04 05:27:10 +0100

    fix: Issue #552 add CardFAQEntry

--- frontend/pages/organizations/[id]/faq.vue
+++ frontend/pages/organizations/[id]/faq.vue
@@ -4,12 +4,34 @@
   >
     <p>{{ $t("pages.organizations.id.faq.subheader") }} {{ id }}</p>
   </div>
+
+  <CardFAQEntry :faqEntry="faqEntry_01" />
+  <CardFAQEntry :faqEntry="faqEntry_02" />
+  <CardFAQEntry :faqEntry="faqEntry_03" />
 </template>
 
 <script setup lang="ts">
+import { FaqEntry } from "../../../types/card-faq-entry";
+
 definePageMeta({
   layout: "sidebar",
 });
 
 const { id } = useRoute().params;
+
+const faqEntry_01: FaqEntry = {
+  question: "FAQ question text 01",
+  answer:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras feugiat bibendum libero in condimentum. Pellentesque euismod consequat mi ac mollis. In viverra, orci a consequat varius, nisi sem dictum ex, id fermentum purus quam non risus. Curabitur sit amet sem mollis, iaculis felis eu, viverra urna.",
+};
+const faqEntry_02: FaqEntry = {
+  question: "FAQ question text 02",
+  answer:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras feugiat bibendum libero in condimentum. Pellentesque euismod consequat mi ac mollis. In viverra, orci a consequat varius, nisi sem dictum ex, id fermentum purus quam non risus. Curabitur sit amet sem mollis, iaculis felis eu, viverra urna.",
+};
+const faqEntry_03: FaqEntry = {
+  question: "FAQ question text 03",
+  answer:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras feugiat bibendum libero in condimentum. Pellentesque euismod consequat mi ac mollis. In viverra, orci a consequat varius, nisi sem dictum ex, id fermentum purus quam non risus. Curabitur sit amet sem mollis, iaculis felis eu, viverra urna.",
+};
 </script>
