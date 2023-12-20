93b1ca18791c080930ea8d1336256e610760b284
José Rigoberto Salas Chacón <rigojunnior@gmail.com>
2023-12-20 10:35:55 -0500

    feat: add the emits and maximum organization count

--- frontend/components/card/CardOrgApplicationVote.vue
+++ frontend/components/card/CardOrgApplicationVote.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="card-style py-10 px-5">
+  <div class="card-style px-10 py-5">
     <h3 class="text-left responsive-h3 font-display">{{ title }}</h3>
     <div class="py-3 flex flex-row">
-      <div v-for="organization in organizations" class="mr-5">
+      <div v-for="organization in availableOrganizations" class="mr-5">
         <Icon
           v-if="organization.imageURL === undefined"
           name="IconOrganization"
-          size="1.25em"
+          size="3em"
         />
-        <img v-else :src="organization.imageURL" />
+        <img v-else :src="organization.imageURL" class="w-12 h-full" />
       </div>
     </div>
     <div class="flex">
       <BtnLabeled
-        @click="upVotes++"
+        @click="$emit('up-vote')"
         class="flex mr-5"
         :cta="true"
         :label="`${upVotes}`"
@@ -24,7 +24,7 @@
         :disabled="isVotingDisabled"
       />
       <BtnLabeled
-        @click="downVotes++"
+        @click="$emit('down-vote')"
         class="flex"
         :cta="true"
         :label="`${downVotes}`"
@@ -59,13 +59,38 @@ export interface Props {
    * Indicates whether the voting should be disabled or not.
    */
   isVotingDisabled?: boolean;
+
+  /**
+   * The up votes count.
+   */
+  upVotes: number;
+
+  /**
+   * The down votes count.
+   */
+  downVotes: number;
 }
 
-// TODO: for testing purpose.
-const upVotes = ref(123);
-const downVotes = ref(123);
+/**
+ * The available component events.
+ */
+export interface Emits {
+  /**
+   * The up vote casted event.
+   */
+  (event: 'up-vote'): void;
 
-withDefaults(defineProps<Props>(), {
+  /**
+   * The down vote casted event.
+   */
+  (event: 'down-vote'): void;
+}
+
+defineEmits<Emits>();
+const props = withDefaults(defineProps<Props>(), {
   isVotingDisabled: true,
 });
+
+const maximumCountOrganizations = 10;
+const availableOrganizations = computed(() => props.organizations.slice(0, maximumCountOrganizations));
 </script>
