698112c4b22c9ab0ab607119782385fa5d4e9880
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-02-02 14:47:20 +0100

    #679 allow for nominatim requests to set map markers

--- frontend/components/media/MediaMap.vue
+++ frontend/components/media/MediaMap.vue
@@ -42,139 +42,143 @@ function isWebglSupported() {
   return false;
 }
 
-function initializeMap() {
-  if (!isWebglSupported()) {
-    alert(i18n.t("components.media-map.maplibre-gl-alert"));
-  } else {
-    const map = new maplibregl.Map({
-      container: "map",
-      style: {
-        version: 8,
-        sources: {
-          "raster-tiles": {
-            type: "raster",
-            tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-            tileSize: 256,
-            attribution:
-              '<a href="https://www.openstreetmap.org/about" target="_blank">Data &copy; OpenStreetMap contributors</a>',
-          },
-        },
-        layers: [
-          {
-            id: "background",
-            type: "background",
-            paint: {
-              "background-color":
-                colorMode.preference == "dark" ? "#131316" : "#F6F8FA",
+onMounted(() => {
+  const nominatimLocationRequest =
+    "https://nominatim.openstreetmap.org/search?q=Brandenburg%20Gate%20Berlin&format=json";
+
+  fetch(nominatimLocationRequest)
+    .then((response) => response.json())
+    .then((data) => {
+      const location = data[0];
+      if (!isWebglSupported()) {
+        alert(i18n.t("components.media-map.maplibre-gl-alert"));
+      } else {
+        const map = new maplibregl.Map({
+          container: "map",
+          style: {
+            version: 8,
+            sources: {
+              "raster-tiles": {
+                type: "raster",
+                tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+                tileSize: 256,
+                attribution:
+                  '<a href="https://www.openstreetmap.org/about" target="_blank">Data &copy; OpenStreetMap contributors</a>',
+              },
             },
+            layers: [
+              {
+                id: "background",
+                type: "background",
+                paint: {
+                  "background-color":
+                    colorMode.preference == "dark" ? "#131316" : "#F6F8FA",
+                },
+              },
+              {
+                id: "simple-tiles",
+                type: "raster",
+                source: "raster-tiles",
+                minzoom: 0,
+                maxzoom: 24,
+              },
+            ],
           },
-          {
-            id: "simple-tiles",
-            type: "raster",
-            source: "raster-tiles",
-            minzoom: 0,
-            maxzoom: 24,
-          },
-        ],
-      },
-      center: [13.38, 52.5168],
-      zoom: 15,
-      pitch: 20,
-    });
+          center: [parseFloat(location["lon"]), parseFloat(location["lat"])],
+          zoom: 15,
+          pitch: 20,
+        });
 
-    map.addControl(
-      new maplibregl.NavigationControl({
-        visualizePitch: true,
-      }),
-      "top-left"
-    );
-    map.addControl(new maplibregl.FullscreenControl());
-    map.addControl(
-      new maplibregl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      })
-    );
-
-    new maplibregl.Marker({ color: `${props.markerColors[0]}` })
-      .setLngLat([13.3778, 52.5163])
-      .addTo(map);
-
-    map.on("load", () => {
-      const layers = layersFactory(
-        isTouchDevice ? 1.5 : 1,
-        isTouchDevice ? 2 : 1
-      );
-
-      const directions = new MapLibreGlDirections(map, {
-        requestOptions: {
-          alternatives: "true",
-        },
-        layers,
-      });
-
-      directions.interactive = true;
-
-      document.addEventListener("keydown", (event) => {
-        if (event.key === "x") {
-          directions.clear();
-        }
-      });
-
-      const clearDirectionsTextColor =
-        colorMode.preference == "dark" ? "white" : "black";
-
-      // const clearDirectionsControl = `
-      //   <div style="
-      //     background-color: rgba(255, 255, 255, 1);
-      //     padding: 1px 5px;
-      //     margin: 10px;
-      //     border-radius: 5px;
-      //     box-shadow: 0 0 1px 2px rgba(0, 0, 0, 0.15);
-      //     color: ${clearDirectionsTextColor};"
-      //   >
-      //     Clear directions
-      //   </div>
-      // `;
-
-      const clearDirectionsHotkeyControl = `
-        <div style="
-          background-color: rgba(255, 255, 255, 1);
-          padding: 1px 5px;
-          margin: 10px;
-          border-radius: 5px;
-          box-shadow: 0 0 1px 2px rgba(0, 0, 0, 0.15);
-          color: ${clearDirectionsTextColor};"
-        >
-          Clear directions [x]
-        </div>
-      `;
-
-      if (window.innerWidth >= 768) {
         map.addControl(
-          {
-            onAdd: function () {
-              const div = document.createElement("div");
-              // if (window.innerWidth < 768) {
-              //   div.innerHTML = clearDirectionsControl;
-              // } else {
-              //   div.innerHTML = clearDirectionsHotkeyControl;
-              // }
-              div.innerHTML = clearDirectionsHotkeyControl;
-              return div;
-            },
-            onRemove: function () {},
-          },
-          "bottom-left"
+          new maplibregl.NavigationControl({
+            visualizePitch: true,
+          }),
+          "top-left"
         );
+        map.addControl(new maplibregl.FullscreenControl());
+        map.addControl(
+          new maplibregl.GeolocateControl({
+            positionOptions: {
+              enableHighAccuracy: true,
+            },
+            trackUserLocation: true,
+          })
+        );
+
+        new maplibregl.Marker({ color: `${props.markerColors[0]}` })
+          .setLngLat([parseFloat(location["lon"]), parseFloat(location["lat"])])
+          .addTo(map);
+
+        map.on("load", () => {
+          const layers = layersFactory(
+            isTouchDevice ? 1.5 : 1,
+            isTouchDevice ? 2 : 1
+          );
+
+          const directions = new MapLibreGlDirections(map, {
+            requestOptions: {
+              alternatives: "true",
+            },
+            layers,
+          });
+
+          directions.interactive = true;
+
+          document.addEventListener("keydown", (event) => {
+            if (event.key === "x") {
+              directions.clear();
+            }
+          });
+
+          const clearDirectionsTextColor =
+            colorMode.preference == "dark" ? "white" : "black";
+
+          // const clearDirectionsControl = `
+          //   <div style="
+          //     background-color: rgba(255, 255, 255, 1);
+          //     padding: 1px 5px;
+          //     margin: 10px;
+          //     border-radius: 5px;
+          //     box-shadow: 0 0 1px 2px rgba(0, 0, 0, 0.15);
+          //     color: ${clearDirectionsTextColor};"
+          //   >
+          //     Clear directions
+          //   </div>
+          // `;
+
+          const clearDirectionsHotkeyControl = `
+          <div style="
+            background-color: rgba(255, 255, 255, 1);
+            padding: 1px 5px;
+            margin: 10px;
+            border-radius: 5px;
+            box-shadow: 0 0 1px 2px rgba(0, 0, 0, 0.15);
+            color: ${clearDirectionsTextColor};"
+          >
+            Clear directions [x]
+          </div>
+        `;
+
+          if (window.innerWidth >= 768) {
+            map.addControl(
+              {
+                onAdd: function () {
+                  const div = document.createElement("div");
+                  // if (window.innerWidth < 768) {
+                  //   div.innerHTML = clearDirectionsControl;
+                  // } else {
+                  //   div.innerHTML = clearDirectionsHotkeyControl;
+                  // }
+                  div.innerHTML = clearDirectionsHotkeyControl;
+                  return div;
+                },
+                onRemove: function () {},
+              },
+              "bottom-left"
+            );
+          }
+        });
       }
     });
-  }
-}
-
-onMounted(() => {
-  initializeMap();
 });
 </script>
