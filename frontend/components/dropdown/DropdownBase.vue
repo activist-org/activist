ef7e88eca8d01bffa8591234b16e90b2ea36116b
Tanya Kharitonovich <k.tanya@tut.by>
2024-02-15 09:34:24 +0100

    feat: create `DropdownBase`, `DropdownCreate`, `DropdownInfo`, `DropdownItemsLayout`, `DropdownUserOptions`, `MenuItemLabel`

---
+++ frontend/components/dropdown/DropdownBase.vue
@@ -0,0 +1,143 @@
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton
+        v-slot="{ open }"
+        class="inline-flex w-full py-2 px-4 font-semibold select-none rounded-md style-btn"
+        :class="{
+          'pl-6': isSideMenu,
+          'flex items-center pl-1 rounded-md style-menu-option-cta': isSideLeftMenu,
+        }"
+        :aria-label="$t(`${menuButtonAriaLabel}`)"
+      >
+        <div
+          class="flex justify-between items-center text-sm space-x-2"
+          :class="{
+          'relative z-0 w-full pl-[0.625rem] font-medium text-left': isSideLeftMenu
+        }"
+        >
+          <div class="flex items-center space-x-2">
+            <Icon
+              :name=menuButtonIcon
+              :class="{
+                'flex-shrink-0 w-5 h-5 text-center': isSideLeftMenu,
+              }"
+              :size="isSideLeftMenu ? '1em' : ''"
+            />
+            <Transition name="text">
+              <p
+                v-if="
+                  !isSideLeftMenu
+                  || sidebar.collapsed == false
+                  || sidebar.collapsedSwitch == false
+                "
+                :class="{
+                  'sr-only lg:not-sr-only': !isSideLeftMenu,
+                  '!not-sr-only !ml-3': isSideMenu,
+                  'uppercase': isMenuButtonUppercase,
+                  'font-bold': isMenuButtonBold,
+                  'select-none': isSideLeftMenu,
+                }"
+              >
+                {{ menuButtonLabel }}
+              </p>
+            </Transition>
+          </div>
+          <Transition name="chevron">
+            <Icon
+              v-if="
+                !isSideLeftMenu
+                || sidebar.collapsed == false
+                || sidebar.collapsedSwitch == false
+              "
+              class="right-3"
+              :class="{
+                'rotate-180 transform': open,
+                absolute: isSideMenu,
+                'absolute right-2': isSideLeftMenu,
+              }"
+              :size="isSideLeftMenu ? '1rem' : ''"
+              :name="isSideLeftMenu ? 'bi:chevron-up' : 'bi:chevron-down'"
+            />
+          </Transition>
+          
+        </div>
+      </MenuButton>
+    </div>
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="opacity-0 transform scale-95"
+      enter-to-class="opacity-100 transform scale-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="opacity-100 transform scale-100"
+      leave-to-class="opacity-0 transform scale-95"
+    >
+      <MenuItems
+        class="rounded-md"
+        :class="{
+          'absolute right-0 mt-2 shadow-lg origin-top-right divide-y bg-light-content dark:bg-dark-content ring-1 ring-black ring-opacity-5 focus:outline-none dark:border dark:border-dark-text': !isSideLeftMenu,
+          '!static': isSideMenu || isSideLeftMenu,
+          'p-1 mt-1 bg-light-header dark:bg-dark-header': isSideLeftMenu,
+        }"
+      >
+        <slot />
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItems } from "@headlessui/vue";
+import { DropdownLocation } from "~/types/location";
+
+
+const props = defineProps<{
+  location?: DropdownLocation;
+  menuButtonAriaLabel: string;
+  menuButtonIcon: string;
+  isMenuButtonUppercase?: boolean;
+  isMenuButtonBold?: boolean;
+  menuButtonLabel: string;
+}>();
+
+const sidebar = useSidebar();
+
+const isSideLeftMenu = computed(() => {
+  return props.location === DropdownLocation.SIDELEFTMENU
+});
+
+const isSideMenu = computed(() => {
+  return props.location === DropdownLocation.SIDEMENU
+});
+</script>
+
+<style>
+.text-enter-active {
+  transition: opacity 0.25s ease;
+  transition-delay: 0.125s;
+}
+.text-leave-active {
+  transition: opacity 0.25s ease;
+}
+
+.text-enter-from,
+.text-leave-to {
+  opacity: 0;
+}
+.text-enter-from {
+  transition-delay: 0.25s;
+}
+
+.chevron-enter-active {
+  transition: opacity 0.25s ease;
+  transition-delay: 0.25s;
+}
+.chevron-leave-active {
+  transition: opacity 0.25s ease;
+}
+
+.chevron-enter-from,
+.chevron-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ frontend/components/menu/MenuItemLabel.vue
@@ -0,0 +1,84 @@
+<template>
+  <component
+    :is="infoComponent.is"
+    v-bind="infoComponent"
+    class="flex items-center text-sm w-full"
+    :class="{
+      'group [word-spacing:0.5em] rounded-md pl-4 pr-3 py-2': !isSideLeftMenu,
+      'bg-light-cta-orange/80 dark:bg-dark-cta-orange/25 dark:text-dark-cta-orange':
+        active && !isSideLeftMenu,
+      'text-light-text dark:text-dark-text': !active,
+      'relative z-0 font-medium text-left space-x-2': isSideLeftMenu, 
+    }"
+  >
+    <Icon
+      v-if="iconName"
+      :name="iconName"
+      size="1em"
+      :class="{'flex-shrink-0 w-5 h-5': isSideLeftMenu}"
+    />
+    <Transition>
+      <component
+        v-if="
+          !isSideLeftMenu
+          || sidebar.collapsed == false
+          || sidebar.collapsedSwitch == false
+        "
+        :is="infoLabel.is"
+        v-bind="infoLabel"          
+      >
+        {{ label }}                 
+      </component>
+    </Transition>      
+  </component>
+</template>
+  
+<script setup lang="ts">
+
+const props = defineProps<{
+  isSideLeftMenu?: boolean | undefined;
+  isButton: boolean;
+  handlerClick?: () => void | undefined;
+  iconName?: string | undefined;
+  ariaLabel?: string | undefined;
+  label: string;
+  active: any;
+}>();
+
+const sidebar = useSidebar();
+
+const infoComponent = computed(() => {
+  return props.isButton
+    ? {
+        is: 'button',
+        onclick: props.handlerClick,
+        ariaLabel: props.ariaLabel,
+      }
+    : {
+        is: 'li',
+      }
+});
+
+const infoLabel = computed(() => {
+  return props.isButton
+    ? {
+        is: 'p',
+        class: 'pl-2 pr-2',
+      }
+    : props.isSideLeftMenu 
+      ? {
+          is: 'p',
+          class: 'select-none whitespace-nowrap hover:light-menu-selection',
+        }
+      : props.iconName 
+        ? {
+            is: 'p',
+            class: 'pl-2 pr-2',
+          }
+        : {
+          is: 'span',
+          }
+});
+
+</script>
+  
