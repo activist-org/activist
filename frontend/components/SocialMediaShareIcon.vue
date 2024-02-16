a9d61afa72f13a74d39a7336f31eab88ff3d120b
Tessa <anderson.quintessa@gmail.com>
2024-02-15 23:36:42 -0600

    abstracted share icon logic into a re-usable component

---
+++ frontend/components/SocialMediaShareIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="cursor-pointer"
+    style="width: 100%; height: 100%; justify-content: flex-start; align-items: center; gap: 12px; display: inline-flex">
+    <div style="iconStyle">
+      <svg width="32" height="32" viewBox="0 0 16 16" :fill="fillColor" xmlns="http://www.w3.org/2000/svg">
+        <path :d="iconPath" />
+      </svg>
+    </div>
+    <div>{{ text }}</div>
+  </div>
+</template>
+  
+<script>
+
+const iconStyle = {
+  width: '28px', height: '28px', position: 'relative'
+};
+
+export default {
+  props: {
+    iconPath: String,
+    fillColor: String,
+    text: String,
+    onClick: Function,
+    iconStyle: iconStyle,
+    textStyle: Object,
+  }
+};
+</script>
