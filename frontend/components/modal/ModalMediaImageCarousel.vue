8a903d5ab6e5679977bc97f9146f171ee63bcc2f
Nicholas Vecchio <88710714+nicholas-vecchio@users.noreply.github.com>
2024-03-16 15:18:37 +1100

    Adjusted modal base scope.
    Changed the scope of the modal base component so that only the fullscreen button will trigger the modal, instead of all components.

--- frontend/components/modal/ModalMediaImageCarousel.vue
+++ frontend/components/modal/ModalMediaImageCarousel.vue
@@ -1,15 +1,23 @@
 <template>
     <div class='relative'>
+        <!-- Media carousel displayed by default -->
+        <MediaImageCarousel :fullscreen='false' :class="{ 'lg:hidden': textExpanded }" />
+
+        <!-- Placeholder add button -->
+        <button class="absolute right-2 bottom-2 z-10">
+            <Icon class="w-12 h-12 border-solid border border-white border-opacity-80 bg-black bg-opacity-80 rounded-lg" name="bi:plus-lg" />                
+        </button>
+
+        <!-- ModalBase component -->
         <ModalBase>
+            <!-- Normal display slot -->
             <template #normalDisplay>
                 <button class="absolute right-2 top-2 z-10">
-                    <Icon class="w-12 h-12 border-solid border border-white/80 bg-black/80 rounded-lg" name="ic:outline-fullscreen" />                 
+                    <Icon class="w-12 h-12 border-solid border border-white border-opacity-80 bg-black bg-opacity-80 rounded-lg" name="ic:outline-fullscreen" />                 
                 </button>
-                <button class="absolute right-2 bottom-2 z-10">
-                    <Icon class="w-12 h-12 border-solid border border-white/80 bg-black/80 rounded-lg" name="bi:plus-lg" />                
-                </button>
-                <MediaImageCarousel :fullscreen='false' :class="{ 'lg:hidden': textExpanded }" />
             </template>
+
+            <!-- Modal display slot -->
             <template #modalDisplay>
                 <MediaImageCarousel :fullscreen='true' :class="{ 'lg:hidden': textExpanded }" />
             </template>
