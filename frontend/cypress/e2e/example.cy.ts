69bac909d6c80b98fcd23d3b7bb51ea0a796d727
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-03-16 20:30:08 +0100

    Change config file to TS and fix formatting errors

--- frontend/cypress/e2e/example.cy.ts
+++ frontend/cypress/e2e/example.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
