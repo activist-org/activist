41da5a4814020827276aa17487741e6a6efe4139
Burak Özmen <ozmen.brk@gmail.com>
2024-03-21 23:14:46 +0100

    746: add multiple image uploading

---
+++ frontend/components/FileDropZone.vue
@@ -0,0 +1,68 @@
+<template>
+  <div @drop.prevent="onDrop"
+    @dragenter.prevent="activate"
+    @dragover.prevent="activate"
+    @dragleave.prevent="deactivate"
+    :class="{
+      'bg-primary': isActive,
+      'bg-light-layer-1 dark:bg-dark-layer-1': !isActive
+    }"
+  >
+    <label for="file-input" class="h-96 mt-6 block card-style flex items-center justify-center flex-col">
+
+    <slot :isDropZoneActive="isActive" ></slot>
+    <input id="file-input" class="hidden" type="file" multiple @change="onInputChange" />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+const emit = defineEmits(['files-dropped']);
+
+const isActive = ref(false);
+
+let deactivateTimeoutKey: number | null = null;
+
+const activate = () => {
+    console.log('activate');
+    isActive.value = true;
+    if (deactivateTimeoutKey) clearTimeout(deactivateTimeoutKey);
+}
+
+const deactivate = () => {
+    isActive.value = false;
+    deactivateTimeoutKey = window.setTimeout(() => {
+        isActive.value = false;
+    }, 50);
+}
+
+function onDrop(e: DragEvent) {
+  if (!e.dataTransfer) return;
+  emit('files-dropped', [...e.dataTransfer.files])
+}
+
+function onInputChange(e: Event) {
+  const target = e.target as HTMLInputElement;
+  if (!target.files) return;
+  emit('files-dropped', [...target.files]);
+}
+
+function preventDefaults(e: Event) {
+    e.preventDefault()
+}
+
+const events = ['dragenter', 'dragover', 'dragleave', 'drop']
+
+onMounted(() => {
+    events.forEach((eventName) => {
+        document.body.addEventListener(eventName, preventDefaults)
+    })
+})
+
+onUnmounted(() => {
+    events.forEach((eventName) => {
+        document.body.removeEventListener(eventName, preventDefaults)
+    })
+})
+</script>
--- frontend/components/modal/ModalUploadImage.vue
+++ frontend/components/modal/ModalUploadImage.vue
@@ -5,33 +5,78 @@
     </template>
     <template #modalDisplay>
       <div>
-        <img v-if="previewImage" :src="previewImage" class="uploading-image" alt="preview image"/>
-        <label for="upload-image" class="font-bold responsive-h4">Upload Image</label>
-        <input @change="uploadImage" id="upload-image" type="file" accept="image/jpeg">
-        <button v-if="previewImage" @click="saveImage" class="bg-primary rounded-lg p-2">Save</button>
+        <DialogTitle class="font-display">
+          <p class="font-bold responsive-h2">
+            Upload Image
+          </p>
+        </DialogTitle>
+        <div class="mt-2">
+          <FileDropZone @files-dropped="handleFiles" v-slot="{ isDropZoneActive }">
+              <span v-if="isDropZoneActive">
+                  <span>Drop Them Here</span>
+                  <span class="smaller"> to add them</span>
+              </span>
+              <span v-else>
+                  <span>Drag Your Files Here</span>
+                  <span class="smaller">
+                      or <strong><em>click here</em></strong> to select files
+                  </span>
+              </span>
+          </FileDropZone>
+          <p>Number of files: {{ files.length }}</p>
+          <div class="flex flex-row">
+            <span v-for="file in files" class="p-2">
+              <button @click="removeFile(file)" class="text-red-500">X</button>
+              <img
+                :key="file.name"
+                :src="file.url"
+                class="w-28 h-28 object-contain"
+                :alt="'uploaded file' + file.name"
+              />
+            </span>
+          </div>
+        </div>
       </div>
     </template>
   </ModalBase>
 </template>
 
 <script setup lang="ts">
+import { DialogTitle } from '@headlessui/vue';
 import { ref } from 'vue';
 import ModalBase from '~/components/modal/ModalBase.vue';
 
-const previewImage = ref();
-const uploadImage = (e: Event) => {
-  const image = (e.target as HTMLInputElement)?.files?.[0];
-  const reader = new FileReader();
-  if (image) {
-    reader.readAsDataURL(image);
+const files = ref<UploadableFile[]>([]);
+  function handleFiles(newFiles: File[]) {
+        const newUploadableFiles = [...newFiles]
+            .map((file) => new UploadableFile(file))
+            .filter((file) => !fileExists(file.id))
+        files.value = [...files.value, ...newUploadableFiles];
   }
-  reader.onload = (e) => {
-    if (e.target) {
-      previewImage.value = e.target.result;
+
+  function fileExists(otherId: string) {
+      return files.value.some(({ id }) => id === otherId)
+  }
+
+  function removeFile(file: UploadableFile) {
+      const index = files.value.indexOf(file)
+
+      if (index > -1) files.value.splice(index, 1)
+  }
+
+
+class UploadableFile {
+file: File;
+url: string;
+name: string;
+status: null;
+id: string;
+    constructor(file: File) {
+        this.file = file
+        this.name = file.name
+        this.id = `${file.name}-${file.size}-${file.lastModified}-${file.type}`
+        this.url = URL.createObjectURL(file)
+        this.status = null
     }
-  };
-};
-const saveImage = () => {
-  console.log('save image', previewImage.value);
 }
 </script>
