ba784163577fe447960ead55954911090f9fb222
Sean Leong <seanleong888@gmail.com>
2024-03-29 04:53:33 +0000

    feat: grid filter tags with a11y

--- frontend/components/grid/GridFilterTags.vue
+++ frontend/components/grid/GridFilterTags.vue
@@ -1,18 +1,56 @@
 <template>
   <div v-if="tags && tags.length > 0" class="h-fit w-full">
     <div class="flex space-x-3">
-      <button
-        v-for="t in tags"
-        class="elem-shadow-sm focus-brand flex rounded-md border-[1.25px] border-light-text bg-light-layer-2 px-3 py-1 text-light-text hover:bg-black/10 dark:border-dark-text dark:bg-dark-layer-2 dark:text-dark-text dark:hover:bg-white/20"
-      >
-        {{ t }}
-      </button>
+      <div v-for="(option, index) in tags" ref="switches" :key="index" class="flex items-center">
+        <Switch v-model="selected[option]" @keydown="keyboardEvent($event)" v-slot="{ checked }" as="template">
+          <div
+            class="style-cta elem-shadow-sm flex h-max w-max cursor-pointer items-center justify-between space-x-2 border rounded-lg py-1 px-4 sm:py-0 sm:px-2"
+            :class="{
+              'style-cta': checked,
+              'style-cta-secondary': !checked,
+            }"
+          >
+            <div class="flex items-center">
+              <p class="select-none text-center text-base font-bold">
+                {{ option }}
+              </p>
+            </div>
+            <Icon v-if="checked" name="bi:x-lg" size="20" />
+          </div>
+        </Switch>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { Switch } from '@headlessui/vue'
 defineProps<{
-  tags?: string[];
+  tags: string[];
 }>();
+
+interface SelectedOptions {
+  [key: string]: boolean;
+}
+
+const selected = ref<SelectedOptions>({})
+const switches = ref<(HTMLInputElement | null)[]>([]);
+
+const keyboardEvent = (e: KeyboardEvent) => {
+  const currentIndex = switches.value.findIndex(s => s === document.activeElement?.parentElement);
+  const previousIndex = (currentIndex - 1 + switches.value.length) % switches.value.length;
+  const nextIndex = (currentIndex + 1) % switches.value.length;
+  
+  switch (e.code) {
+    case "ArrowUp":
+    case "ArrowLeft":
+      (switches.value[previousIndex]?.childNodes[2] as HTMLInputElement).focus();
+      break;
+    case "ArrowDown":
+    case "ArrowRight":
+      (switches.value[nextIndex]?.childNodes[2] as HTMLInputElement).focus();
+      break;
+  }
+};
 </script>
