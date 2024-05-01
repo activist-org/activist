5bafaa1dbc47390c7ff2724db03d453747f10b78
tosta <tobias.stach@gmail.com>
2024-05-01 11:25:14 +0200

    added login function and changed serializer to use username instead of email

--- frontend/pages/auth/sign-in.vue
+++ frontend/pages/auth/sign-in.vue
@@ -1,42 +1,26 @@
 <template>
   <div class="px-4 sm:px-6 md:px-8 xl:px-24 2xl:px-36">
-    <form class="space-y-4">
+    <form @submit.prevent="signIn" class="space-y-4">
       <div class="col">
-        <FormTextField
-          @update:model-value="userNameValue = $event"
-          :placeholder="$t('pages.auth.sign-in.index.enter-user-name')"
-          :model-value="userNameValue"
-        />
+        <FormTextField @update:model-value="userNameValue = $event"
+          :placeholder="$t('pages.auth.sign-in.index.enter-user-name')" :model-value="userNameValue" />
       </div>
       <div>
-        <FormTextField
-          @update:model-value="passwordValue = $event"
-          :placeholder="$t('components._global.enter-password')"
-          :is-icon-visible="true"
-          input-type="password"
-          :model-value="passwordValue"
-          :icons="['bi:eye-fill']"
-        />
+        <FormTextField @update:model-value="passwordValue = $event"
+          :placeholder="$t('components._global.enter-password')" :is-icon-visible="true" input-type="password"
+          :model-value="passwordValue" :icons="['bi:eye-fill']" />
       </div>
       <IndicatorPasswordStrength :password-value="passwordValue" />
       <div class="flex flex-col space-y-3">
         <FriendlyCaptcha />
-        <BtnAction
-          @click="signIn"
-          class="flex max-h-[48px] w-[116px] items-center justify-center truncate md:max-h-[40px] md:w-[96px]"
-          :label="$t('_global.sign-in')"
-          :cta="true"
-          fontSize="lg"
-          :ariaLabel="$t('components.btn-route-internal.sign-in-aria-label')"
-        />
+        <BtnAction class="flex max-h-[48px] w-[116px] items-center justify-center truncate md:max-h-[40px] md:w-[96px]"
+          :label="$t('_global.sign-in')" :cta="true" fontSize="lg"
+          :ariaLabel="$t('components.btn-route-internal.sign-in-aria-label')" />
       </div>
       <div class="flex pt-4 md:justify-center md:pt-6 lg:pt-8">
         <h6>{{ $t("pages.auth.sign-in.index.no-account") }}</h6>
-        <NuxtLink
-          :to="localePath('/auth/sign-up')"
-          class="link-text ml-2 font-extrabold"
-          >{{ $t("_global.sign-up") }}</NuxtLink
-        >
+        <NuxtLink :to="localePath('/auth/sign-up')" class="link-text ml-2 font-extrabold">{{ $t("_global.sign-up") }}
+        </NuxtLink>
       </div>
     </form>
   </div>
@@ -52,7 +36,25 @@ definePageMeta({
 const userNameValue = ref("");
 const passwordValue = ref("");
 
-const signIn = () => {
+const signIn = async () => {
   //
+  const { data: responseData } = await $fetch(`http://localhost:8000/v1/auth/login/`, {
+    method: "POST",
+    body: JSON.stringify({
+      username: userNameValue.value,
+      password: passwordValue.value,
+    }),
+    onResponse({ request, response, options }) {
+      // Handle the response
+      console.log(
+        "Response:",
+        response.status,
+        response.statusText,
+        response._data
+      );
+    },
+  })
+  console.log();
 };
+
 </script>
