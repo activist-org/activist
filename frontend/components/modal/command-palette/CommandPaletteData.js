004e5c460ae11ca1d830ae7143b48aaa5862bf8c
Matt Burnett <mburnett@tutanota.de>
2024-07-02 18:59:53 +0200

    Basic search and nav now work. See TODO entries for thiings that need fixing / improvement."

--- frontend/components/modal/command-palette/CommandPaletteData.js
+++ frontend/components/modal/command-palette/CommandPaletteData.js
@@ -1,51 +1,58 @@
 export const paletteData = [
   {
     id: 7,
-    category: 'home',
-    path: 'home',
-    iconName: 'HOME',
-    displayName: 'home'
+    category: "home",
+    path: "home",
+    iconName: "HOME",
+    displayName: "_global.home",
+    action: () => console.log("home"),
   },
   {
     id: 1,
-    category: 'discussions',
-    path: 'discussions',
-    iconName: 'DISCUSSION',
-    displayName: 'discussions'
+    category: "discussions",
+    path: "discussions",
+    iconName: "DISCUSSION",
+    displayName: "_global.discussions",
+    action: () => console.log("discussions"),
   },
   {
     id: 2,
-    category: 'events',
-    path: 'events',
-    iconName: 'EVENT',
-    displayName: 'events'
+    category: "events",
+    path: "events",
+    iconName: "EVENT",
+    displayName: "_global.events",
+    action: () => console.log("events"),
   },
   {
     id: 3,
-    category: 'notifications',
-    path: 'notifications',
-    iconName: 'BELL',
-    displayName: 'notifications'
+    category: "notifications",
+    path: "notifications",
+    iconName: "BELL",
+    displayName: "_global.notifications",
+    action: () => console.log("notifications"),
   },
   {
     id: 4,
-    category: 'organizations',
-    path: 'organizations',
-    iconName: 'ORGANIZATION',
-    displayName: 'organization-name'
+    category: "organizations",
+    path: "organizations",
+    iconName: "ORGANIZATION",
+    displayName: "_global.organization-name",
+    action: () => console.log("organizations"),
   },
   // {
   //   id: 5,
   //   category: "resources",
   //   path: "resources",
   //   iconName: "RESOURCE",
-  //   displayName: "resources",
+  //   displayName: "_global.resources_lower",
+  //   action: () => console.log('resources')
   // },
   {
     id: 6,
-    category: 'upcoming-events',
-    path: 'upcoming-events',
-    iconName: 'EVENT',
-    displayName: 'upcoming-events'
-  }
-]
+    category: "upcoming-events",
+    path: "upcoming-events",
+    iconName: "EVENT",
+    displayName: "_global.upcoming-events",
+    action: () => console.log("upcoming-events"),
+  },
+];
--- frontend/components/modal/command-palette/ModalCommandPalette.vue
+++ frontend/components/modal/command-palette/ModalCommandPalette.vue
@@ -1,63 +1,70 @@
-<!-- use https://www.youtube.com/watch?v=-jix4KyxLuQ as reference. -->
+<!-- TODO: Search works, but it's pickiing up the literal values of 'displayName' (eg. "_global.home"). 
+              Search should instead look through the rendered values. -->
+<!-- TODO: Figure out how to usefully implement the 'action' elements in the paletteData objects (CommandPaletteData.js).
+              Right now they're not used, and we have three different ways to navigate to a target path.
+              Keep the 'action' thingies because some time in the future we can use them for actions other than just
+                page navigation. 
+-->
+<!-- TODO: Find a way to use a single event handler to replace
+              handleCommand
+              handleClick
+              handleEnter
+-->
+<!-- TODO: Sometimes router.push() loses the i18n part of the path (eg: /en/the.path.name)-->
 
 <template>
-  <!-- TODO: transition / animation? Maybe do this in ModalBase? -->
-  <!-- TODO: ComboboxOption doesn't jumpTo the target link when Enter key is pressed.
-                @click on NuxtLink works, but @keypress.enter doesn't.
-                It looks like the Enter key event is already handled somewhere in the 
-                  ComboboxInput element, but where ever this is handled, it doesn't
-                  jump to the target link.
-                Something like:
-                  @keypress.enter.stop.prevent="handleEnterKey"
-                Tried setting @keypress.enter on different components (CB, CB.OptionItem, the div before NuxtLink, etc), 
-                  but none of them work.
-   -->
   <ModalBase
     @closeModal="handleCloseModal"
     :isOpen="modalShouldClose == false ? modalIsOpen : false"
   >
-    <Combobox as="div" class="space-x-2 divide-y divide-gray-400">
-      <div
-        class="focus-inside elem-shadow-sm my-2.5 flex w-[90%] grow select-none items-center justify-between rounded-md bg-light-layer-2 px-2 py-1 text-left text-light-distinct-text transition duration-200 dark:bg-dark-layer-2 dark:text-dark-distinct-text"
-      >
-        <Icon :name="IconMap.SEARCH" size="1em" class="text-black" />
-        <ComboboxInput
-          @change="searchTerm = $event.target.value"
-          class="h-9 w-full bg-transparent pl-1 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-0"
-          :placeholder="$t('_global.search')"
-        />
-      </div>
-      <ComboboxOptions
-        v-show="searchResults.length > 0"
-        static
-        class="max-h-40 overflow-y-auto pt-2"
-      >
-        <ComboboxOption
-          v-for="data in searchResults"
-          :key="data.id"
-          v-slot="{ active }"
-          :value="data.path"
-          as="template"
+    <!-- MARK: Main element -->
+    <div>
+      <!-- TODO: There should be one, unifed way to handle selection, instead of 'handleCommand' here -->
+      <Combobox v-model="selectedCommand" @change="handleCommand" as="div">
+        <div
+          class="focus-inside elem-shadow-sm my-2.5 flex w-[90%] grow select-none items-center justify-between rounded-md bg-light-layer-2 px-2 py-1 text-left text-light-distinct-text transition duration-200 dark:bg-dark-layer-2 dark:text-dark-distinct-text"
         >
-          <div
-            :class="[
-              'w-6/7 group relative flex items-center space-x-2 rounded-md  py-1',
-              active ? 'bg-light-highlight dark:bg-dark-highlight' : '',
-            ]"
+          <Icon :name="IconMap.SEARCH" size="1em" class="text-black" />
+          <!-- MARK: Search text input. -->
+          <!-- TODO: There should be one, unifed way to handle selection, instead of 'handleEnter' here -->
+          <ComboboxInput
+            @keydown.enter="handleEnter"
+            @change="searchTerm = $event.target.value"
+            class="h-9 w-full bg-transparent pl-1 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-0"
+            :placeholder="$t('_global.search')"
+          />
+        </div>
+        <div v-if="searchTerm.length > 0">
+          <span class="font-bold italic">Searching for:</span> {{ searchTerm }}
+        </div>
+        <!-- MARK: Search results output. -->
+        <ComboboxOptions
+          v-if="searchTerm.length > 0 && filteredCommands.length > 0"
+          class="max-h-40 overflow-y-auto pt-2"
+        >
+          <!-- MARK: Individual search result/s. -->
+          <!-- TODO: There should be one, unifed way to handle selection, instead of 'handleClick' here -->
+          <ComboboxOption
+            v-for="command in filteredCommands"
+            @click="handleClick(command)"
+            v-slot="{ active }"
+            :key="command.id"
+            :value="command"
+            as="li"
           >
-            <!-- TODO: enable Enter key / @keydown.enter="handleKeydownEnter" / usw -->
-            <NuxtLink
-              @click="handleCloseModal"
-              class="flex w-full items-center"
-              :to="localePath(`/${data.path}`)"
+            <li
+              :class="[
+                'w-6/7 group relative flex items-center space-x-2 rounded-md py-1',
+                active ? 'bg-light-highlight dark:bg-dark-highlight' : '',
+              ]"
             >
               <Icon
-                :name="IconMap[data.iconName as keyof typeof IconMap]"
+                :name="IconMap[command.iconName as keyof typeof IconMap]"
                 size="1em"
-                :alt="$t(`_global.${data.displayName}`)"
+                :alt="$t(`${command.displayName}`)"
               />
               <div class="relative flex-grow pl-1">
-                {{ $t(`_global.${data.displayName}`) }}
+                {{ $t(`${command.displayName}`) }}
                 <span
                   class="link-text invisible absolute right-3 w-16"
                   :class="{ visible: active }"
@@ -65,61 +72,96 @@
                   {{ $t("components.modal-command-palette.jump-to") }}
                 </span>
               </div>
-            </NuxtLink>
-          </div>
-        </ComboboxOption>
-      </ComboboxOptions>
-    </Combobox>
+            </li>
+          </ComboboxOption>
+        </ComboboxOptions>
+        <div v-else-if="searchTerm.length > 0 && filteredCommands.length == 0">
+          No results for: {{ searchTerm }}
+        </div>
+      </Combobox>
+    </div>
   </ModalBase>
 </template>
 
 <script setup lang="ts">
 import { IconMap } from "~/types/icon-map";
+import { useRouter } from "vue-router";
+
 import {
   Combobox,
   ComboboxInput,
   ComboboxOptions,
   ComboboxOption,
 } from "@headlessui/vue";
+import { ref, computed, watch, defineProps, defineEmits } from "vue";
 
-const localePath = useLocalePath();
+// Define props with proper types
+const props = defineProps<{
+  isOpen: boolean;
+  paletteData: Command[];
+}>();
 
-interface PaletteDataItem {
+interface Command {
   id: number;
-  category: string;
   path: string;
   iconName: string;
   displayName: string;
+  action: () => void;
 }
 
-const props = defineProps<{
-  isOpen: boolean;
-  paletteData: PaletteDataItem[];
-}>();
-// TODO: maybe get rid of isOpen, modalIsOpen and modalShouldClose, since all we really
-//  need is the 'closeModal' emit in 'handleCloseModal'
 const modalIsOpen = computed(() => props.isOpen);
 const modalShouldClose = ref(false);
-
 const searchTerm = ref("");
-const searchResults = ref<PaletteDataItem[]>([]);
+const selectedCommand = ref<Command | null>(null);
+const filteredCommands = ref<Command[]>([]);
 
+const router = useRouter();
 const emit = defineEmits(["closeModal"]);
+
+// Handle modal close
 const handleCloseModal = () => {
   searchTerm.value = "";
-  searchResults.value = [];
   emit("closeModal");
 };
-// Watch the searchTerm ref variable
-// TODO: Filter paletteData with useFuse, for fuzzy matching, instead of JS .filter()
-// https://github.com/vueuse/vueuse/blob/main/packages/integrations/useFuse/demo.vue
+
+const handleClick = (command: Command) => {
+  router.push(`/${command.path}`);
+};
+
+// Handle Enter key press
+const handleEnter = () => {
+  if (selectedCommand.value) {
+    // selectedCommand.value.action();
+    router.push(`/${selectedCommand.value.path}`);
+    emit("closeModal");
+  }
+};
+
+// Handle command selection
+// const handleCommand = (command: Command) => {
+const handleCommand = () => {
+  if (selectedCommand.value) {
+    // if (command && typeof command.action === "function") {
+    //   command.action();
+    //   emit("closeModal");
+    // } else {
+    //   console.error(
+    //     "Command action is not a function or command is null",
+    //     command
+    //   );
+    // }
+    router.push(`/${selectedCommand.value.path}`);
+    emit("closeModal");
+  }
+};
+
 watch(searchTerm, (newVal) => {
   if (newVal.length > 0) {
-    searchResults.value = props.paletteData.filter((dataItem) =>
-      dataItem.category.toLowerCase().includes(newVal.toLowerCase())
+    filteredCommands.value = props.paletteData.filter((dataItem) =>
+      dataItem.displayName.toLowerCase().includes(newVal.toLowerCase())
     );
   } else {
-    searchResults.value = [];
+    filteredCommands.value = [];
   }
 });
 </script>
