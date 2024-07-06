0154393be3db32d81e239c2afcd584e403397f8f
tosta <tobias.stach@gmail.com>
2024-07-06 12:15:36 +0200

    added custom management command wait_for_db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "backend",
     "authentication",
     "entities",
     "content",
