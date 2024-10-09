be2a8ba592c3445413d304703f1d634978e7f95c
Sean Leong <seanleong888@gmail.com>
2024-10-09 22:06:43 +0200

    feat: toast alert for signal

--- frontend/components/modal/ModalSharePage.vue
+++ frontend/components/modal/ModalSharePage.vue
@@ -1,4 +1,5 @@
 <template>
+  <Toaster :theme="$colorMode.value === 'dark' ? 'dark' : 'light'" />
   <ModalBase :modalName="modalName">
     <div class="px-2 pb-2 pt-1 lg:px-4 lg:pb-4 lg:pt-2">
       <DialogTitle class="font-display">
@@ -247,6 +248,7 @@ import type { Group } from "~/types/entities/group";
 import type { Organization } from "~/types/entities/organization";
 import type { Event } from "~/types/events/event";
 import { IconMap } from "~/types/icon-map";
+import { toast, Toaster } from "vue-sonner";
 
 const props = defineProps<{
   cta: BtnAction["cta"];
@@ -350,6 +352,7 @@ const copyToClipboardThenOpenURL = async (
   try {
     await navigator.clipboard.writeText(url);
     signalContentCopied.value = true;
+    toast("Opening Signal");
     setTimeout(() => {
       signalContentCopied.value = false;
       if (redirectURL) {
