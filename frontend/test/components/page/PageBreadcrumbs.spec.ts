be92002c6643adac136b3beaf88d7d3077411104
Ashwaq Taib <ashwaqtaib@Ashwaqs-MacBook-Pro.local>
2024-12-11 00:45:03 +0300

    added test cases for testing events in pageBreadcrumbs

---
+++ frontend/test/components/page/PageBreadcrumbs.spec.ts
@@ -0,0 +1,49 @@
+// import render from '~/test/render';
+// import { screen } from '@testing-library/vue';
+// import PageBreadcrumbs from '~/components/page/PageBreadcrumbs.vue';
+// import { useOrganizationStore } from '~/stores/organization';
+// import { useGroupStore } from '~/stores/group';
+// import { useEventStore } from '~/stores/event';
+
+import app from "~/app.vue";
+import render from "@/test/render";
+import { fireEvent, screen, waitFor } from "@testing-library/vue";
+import { registerEndpoint } from "@nuxt/test-utils/runtime";
+import { createError } from "h3";
+
+import PageBreadcrumbs from '~/components/page/PageBreadcrumbs.vue'
+
+// afterEach(() => {
+//   localStorage.removeItem("accessToken");
+//   vi.resetAllMocks();
+// });
+
+const eventsMock = vi.fn();
+registerEndpoint("http://localhost:8000/v1/events/events/18f80f0d-7f73-4158-bf88-495ca0117883/", {
+  method: "POST",
+  handler: eventsMock,
+});
+
+
+describe("pageBreadcrumbs", () => {
+  it("should show the correct route for events", async () => {
+    eventsMock.mockImplementation(() => ({
+        "id": "18f80f0d-7f73-4158-bf88-495ca0117883",
+        "createdBy": "8f846387-c3ea-4c42-9e8c-d592a8b00c8f",
+        "name": "Climate Event o0:e0",
+        "tagline": "area",
+        "iconUrl": null,
+        "type": "learn",
+        "onlineLocationLink": "https://robinson.biz/",
+        "offlineLocationId": "332dd550-3234-4809-9434-6c598dc01b21",
+        "isPrivate": true,
+        "startTime": "2024-12-09T19:39:49.379161Z",
+        "endTime": "2024-12-10T19:39:49.379178Z",
+        "eventText": null
+    }));
+    //window.location.assign("http://localhost:3000/events/18f80f0d-7f73-4158-bf88-495ca0117883");
+    await render(app, { route: '/events/18f80f0d-7f73-4158-bf88-495ca0117883'});
+    const nav = screen.getByRole("nav", { name: "Pae breadcrumb navigation" });
+    screen.debug(nav);
+  });
+});
