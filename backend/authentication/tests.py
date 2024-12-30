5f92e0ea21679c5d804c33b6a0f4a0ef0d3d45f4
tosta <tobias.stach@gmail.com>
2024-12-30 20:56:47 +0100

    updated tests and pytest setting

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -8,9 +8,6 @@ from .factories import (
     SupportEntityTypeFactory,
     SupportFactory,
     UserFactory,
-    UserResourceFactory,
-    UserTaskFactory,
-    UserTopicFactory,
 )
 from .models import UserModel
 
@@ -31,16 +28,10 @@ def test_str_methods() -> None:
     support_entity_type = SupportEntityTypeFactory.build()
     support = SupportFactory.build()
     user = UserFactory.build()
-    user_resource = UserResourceFactory.build()
-    user_task = UserTaskFactory.build()
-    user_topic = UserTopicFactory.build()
 
     assert str(support_entity_type) == support_entity_type.name
     assert str(support) == str(support.id)
     assert str(user) == user.username
-    assert str(user_resource) == str(user_resource.id)
-    assert str(user_task) == str(user_task.id)
-    assert str(user_topic) == str(user_topic.id)
 
 
 def test_sign_up(client: Client) -> None:
