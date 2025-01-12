c42ff588e9bf69202b2180d229340524e382ad77
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-01-12 21:30:22 +0100

    Update frontend env restart process user notifications

--- frontend/reset_local_env.sh
+++ frontend/reset_local_env.sh
@@ -3,21 +3,21 @@
 # Linux: bash reset_local_env.sh
 # Windows: Run the commands below.
 
-echo "Replacing local yarn.lock with the version on the main branch."
+echo "Replacing local yarn.lock with the version on main..."
 wget -O yarn.lock https://raw.githubusercontent.com/activist-org/activist/refs/heads/main/frontend/yarn.lock
 
-echo "Cleaning yarn cache system wide (might take a long time)."
+echo "Cleaning yarn cache system wide. This might take a long time..."
 yarn cache clean
 
-echo "Deleting the local node_modules directory."
+echo "Deleting local node_modules directory..."
 rm -rf node_modules
-echo "Local node_modules was deleted."
+echo "Local node_modules deleted."
 
-echo "Loading environment variables into your shell."
+echo "Loading environment variables into your shell..."
 set -a && source ../.env.dev && set +a
-echo "Environment variables were loaded."
+echo "Environment variables loaded."
 
-echo "Reinstalling frontend dependencies."
+echo "Reinstalling frontend dependencies..."
 yarn install
 
 echo "Local frontend environment restarted."
