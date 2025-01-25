42a6356031bff40848e2672a568c99677cdf5977
Fowlerz <fowlerztwitch@gmail.com>
2025-01-25 23:33:09 +0100

    Add named CSS colors to frontend password strength indicator

--- frontend/components/indicator/IndicatorPasswordStrength.vue
+++ frontend/components/indicator/IndicatorPasswordStrength.vue
@@ -46,19 +46,19 @@ const text = computed(() => passwordStrengthMap[score.value].text);
 
 const passwordStrengthMap: Record<number, { color: string; text: string }> = {
   0: {
-    color: "bg-[#cc0000] dark:bg-[#e06666]",
+    color: "bg-[rgba(var(--password-vweak))] dark:bg-[rgba(var(--password-vweak))]",
     text: "components.indicator_password_strength.very_weak",
   },
   1: {
-    color: "bg-[#e69138] dark:bg-[#f6b26b]",
+    color: "bg-[rgba(var(--password-weak))] dark:bg-[rgba(var(--password-weak))]",
     text: "components.indicator_password_strength.weak",
   },
   2: {
-    color: "bg-[#f1c232] dark:bg-[#ffd966]",
+    color: "bg-[rgba(var(--password-medium))] dark:bg-[rgba(var(--password-medium))]",
     text: "components.indicator_password_strength.medium",
   },
   3: {
-    color: "bg-[#6aa84f] dark:bg-[#93c47d]",
+    color: "bg-[rgba(var(--password-strong))] dark:bg-[rgba(var(--password-strong))]",
     text: "components.indicator_password_strength.strong",
   },
   4: {
