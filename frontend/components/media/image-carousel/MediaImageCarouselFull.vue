60c161814a138fdea828302f418787a873d60927
Matt Burnett <mburnett@tutanota.de>
2025-02-20 23:18:07 +0100

    WIP - Media carousel fullscreen.
    Commenting out / disabling fullscreen display.
    It's not critical functionality and it's taking a long time to sort out.

--- frontend/components/media/image-carousel/MediaImageCarouselFull.vue
+++ frontend/components/media/image-carousel/MediaImageCarouselFull.vue
@@ -2,13 +2,13 @@
 <template>
   <div class="relative">
     <MediaImageCarousel :fullscreen="true" :organizationId="organizationId" />
-    <button
+    <!-- <button
       @click="openModal()"
       @keydown.enter="openModal()"
       class="focus-brand absolute right-2 top-2 z-10 hidden rounded-lg border border-black/80 bg-white/80 p-1 text-black/80 dark:border-white/80 dark:bg-black/80 dark:text-white/80 md:block"
     >
       <Icon :name="IconMap.FULL_SCREEN" size="1.5em" />
-    </button>
+    </button> -->
     <ModalMediaImageCarousel @closeModal="handleCloseModal" />
   </div>
 </template>
