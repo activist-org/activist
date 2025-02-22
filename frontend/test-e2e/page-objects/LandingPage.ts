3da94baed57ba67222403a086b4e7ab4905c666d
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-02-22 19:15:03 +0100

    Change tests to run with new i18n.ts and remove unneeded file

--- frontend/test-e2e/page-objects/LandingPage.ts
+++ frontend/test-e2e/page-objects/LandingPage.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 import type { LOCALE_CODE } from "~/locales";
-import { getLocalText } from "~/utils/localeFileHandler";
+import { getLocaleText } from "~/utils/i18n";
 
 export const newLandingPage = (locale?: LOCALE_CODE) => ({
-  headingText: getLocalText(locale)["i18n.components.landing_splash.header"],
+  headingText: getLocaleText(locale)["i18n.components.landing_splash.header"],
 });
