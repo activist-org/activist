06dfcedac99680385ba97e3b93508b67ff57f4ed
cquinn540 <cquinn540@gmail.com>
2025-03-18 16:08:37 -0400

    feat: component test ofr individual password rules

--- frontend/test/pages/auth/sign-up.spec.ts
+++ frontend/test/pages/auth/sign-up.spec.ts
@@ -4,22 +4,6 @@ import render from "@/test/render";
 import { fireEvent, screen, waitFor, within } from "@testing-library/vue";
 
 describe("sign-up", () => {
-  it("shows errors for one character password", async () => {
-    await render(SignUp);
-
-    const passwordInput = screen.getByPlaceholderText(/enter your password/i);
-
-    await fireEvent.update(passwordInput, "a");
-    await screen.findByText(/for your security/i);
-
-    const line = screen.getByTestId("capital-letters");
-
-    // Icons render delayed
-    const icon = await within(line).findByRole("img", { name: "failed" });
-
-    expect(icon.style.color).toBe("#BA3D3B");
-  });
-
   it("shows error border on blur when password invalid", async () => {
     await render(SignUp);
 
@@ -37,5 +21,66 @@ describe("sign-up", () => {
         screen.getByTestId(/enter your password/i).parentElement!.className
       ).toContain("border-action-red dark:border-action-red");
     });
+  });
+
+  it.each([
+    [
+      "a",
+      [
+        { rule: "capital-letters", passed: false },
+        { rule: "contains-numbers", passed: false },
+        { rule: "contains-special-chars", passed: false },
+        { rule: "lower-case-letters", passed: true },
+        { rule: "number-of-chars", passed: false },
+      ],
+    ],
+    [
+      "ABCEDFGHIJK",
+      [
+        { rule: "capital-letters", passed: true },
+        { rule: "contains-numbers", passed: false },
+        { rule: "contains-special-chars", passed: false },
+        { rule: "lower-case-letters", passed: false },
+        { rule: "number-of-chars", passed: false },
+      ],
+    ],
+    [
+      "#$%",
+      [
+        { rule: "capital-letters", passed: false },
+        { rule: "contains-numbers", passed: false },
+        { rule: "contains-special-chars", passed: true },
+        { rule: "lower-case-letters", passed: false },
+        { rule: "number-of-chars", passed: false },
+      ],
+    ],
+    [
+      "012345678912",
+      [
+        { rule: "capital-letters", passed: false },
+        { rule: "contains-numbers", passed: true },
+        { rule: "contains-special-chars", passed: false },
+        { rule: "lower-case-letters", passed: false },
+        { rule: "number-of-chars", passed: true },
+      ],
+    ],
+  ])("shows rule violations for password: %s", async (password, rules) => {
+    await render(SignUp);
+
+    const passwordInput = screen.getByPlaceholderText(/enter your password/i);
+
+    await fireEvent.update(passwordInput, password);
+    await screen.findByText(/for your security/i);
+
+    for (const { rule, passed } of rules) {
+      const line = screen.getByTestId(rule);
+
+      // Icons render delayed
+      const icon = await within(line).findByRole("img", {
+        name: passed ? "passed" : "failed",
+      });
+
+      expect(icon.style.color).toBe(passed ? "#198754" : "#BA3D3B");
+    }
   });
 });
