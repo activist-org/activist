c66376a02c91f275dc526d34924e27b214ce252c
sharan <sharancodes@gmail.com>
2025-03-25 02:02:57 +0530

    updated tests for update_social_links and few other small changes to other files(formatted).

--- backend/communities/groups/tests/test_update_social_links.py
+++ backend/communities/groups/tests/test_update_social_links.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 import pytest
 from django.test import Client
+from rest_framework.response import Response
 
 from authentication.factories import UserFactory
 from communities.groups.factories import GroupFactory
@@ -12,18 +13,26 @@ pytestmark = pytest.mark.django_db
 """
 Function for posting to backend.
 """
-def post_update(test_uuid: UUID, test_group: GroupFactory, test_order: str, test_link: str, test_label:str):
+
+
+def post_update(
+    test_uuid: UUID,
+    test_group: GroupFactory,
+    test_order: str,
+    test_link: str,
+    test_label: str,
+) -> Response:
     client = Client()
     response = client.put(
         path=f"/v1/communities/group_social_links/{test_uuid}/",
         data={
             "group": test_group,
             "order": test_order,
-            "link" : test_link,
+            "link": test_link,
             "label": test_label,
-        }
+        },
     )
-    
+
     return response
 
 
@@ -39,7 +48,13 @@ def test_update_social_links(client: Client) -> None:
     test_label = "label"
     # 1. Bad UUID returns None
     bad_uuid = uuid.uuid4()
-    response = post_update(test_uuid=bad_uuid, test_group=test_update_group, test_order=test_order, test_link=test_link, test_label=test_label)
+    response = post_update(
+        test_uuid=bad_uuid,
+        test_group=test_update_group,
+        test_order=test_order,
+        test_link=test_link,
+        test_label=test_label,
+    )
     assert response.status_code == 404
 
     # 2. Group ID exists and is updated as authenticated user.
@@ -49,13 +64,15 @@ def test_update_social_links(client: Client) -> None:
 
     group_id = test_update_group.id
     login_response = client.post(
-        path="/v1/auth/sign_in/",
-        data={
-            "email": user.email,
-            "password": user.password
-        }
+        path="/v1/auth/sign_in/", data={"email": user.email, "password": user.password}
     )
-    
+
     if login_response.status_code == 200:
-        response = post_update(test_uuid=group_id, test_group=test_update_group, test_order=test_order, test_link=test_link, test_label=test_label)
+        response = post_update(
+            test_uuid=group_id,
+            test_group=test_update_group,
+            test_order=test_order,
+            test_link=test_link,
+            test_label=test_label,
+        )
         assert response.status_code == 200
