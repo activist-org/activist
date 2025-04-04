fd8cd45bc8e1d792f541e8400e0297bdbd82e7b6
Ian Wang <wangin@uci.edu>
2025-04-04 13:56:55 -0700

    new APIView passes tests

--- backend/communities/groups/views.py
+++ backend/communities/groups/views.py
@@ -5,7 +5,9 @@ from typing import Dict, List
 from uuid import UUID
 
 from django.db import transaction
+from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework import status, viewsets
+from rest_framework.generics import GenericAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -19,6 +21,147 @@ from content.models import Location
 from core.paginator import CustomPagination
 
 # MARK: Main Tables
+
+
+class GroupListAPIView(GenericAPIView[Group]):
+    queryset = Group.objects.all().order_by("id")
+    serializer_class = GroupSerializer
+    pagination_class = CustomPagination
+
+    def get(self, request: Request) -> Response:
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        # print("create", serializer.initial_data)
+        serializer.is_valid(raise_exception=True)
+
+        location_dict = serializer.validated_data["location"]
+        location = Location.objects.create(**location_dict)
+        serializer.validated_data["location"] = location
+
+        serializer.save(created_by=request.user)
+        data = {"message": f"New group created: {serializer.data}."}
+
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class GroupDetailAPIView(GenericAPIView[Group]):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+    @extend_schema(
+        responses={
+            200: GroupSerializer,
+            400: OpenApiResponse(response={"error": "Group ID is required"}),
+            404: OpenApiResponse(response={"error": "Failed to retrieve the Group"}),
+        }
+    )
+    def get(self, request: Request, id: None | UUID = None) -> Response:
+        """
+        Retrieve a single Group by ID.
+        """
+        if id is None:
+            return Response(
+                {"error": "Group ID is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            group = Group.objects.get(id=id)
+            serializer = GroupSerializer(group)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Group.DoesNotExist:
+            return Response(
+                {"error": "Failed to retrieve the Group"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+    @extend_schema(
+        responses={
+            200: GroupSerializer,
+            400: OpenApiResponse(response={"error": "Group ID is required"}),
+            404: OpenApiResponse(response={"error": "Group not found"}),
+        }
+    )
+    def put(self, request: Request, id: None | UUID = None) -> Response:
+        """
+        Update an Group by ID.
+        """
+        if id is None:
+            return Response(
+                {"error": "Group ID is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            group = Group.objects.get(id=id)
+
+        except Group.DoesNotExist:
+            return Response(
+                {"error": "Group not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user != group.created_by and not request.user.is_staff:
+            return Response(
+                {"error": "You are not authorized to update this Group"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        serializer = self.serializer_class(group, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(response={"message": "Group deleted successfully."}),
+            400: OpenApiResponse(response={"error": "Group ID is required"}),
+            401: OpenApiResponse(
+                response={"error": "You are not authorized to delete this Group"}
+            ),
+            404: OpenApiResponse(response={"error": "Group not found"}),
+        }
+    )
+    def delete(self, request: Request, id: None | UUID = None) -> Response:
+        """
+        Delete an Group by ID.
+        """
+        if id is None:
+            return Response(
+                {"error": "Group ID is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            group = Group.objects.select_related("created_by").get(id=id)
+
+        except Group.DoesNotExist:
+            return Response(
+                {"error": "Group not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user != group.created_by and not request.user.is_staff:
+            return Response(
+                {"error": "You are not authorized to delete this Group"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        # group.status = StatusType.objects.get(id=3)  # 3 is the id of the deleted status
+        # group.deletion_date = timezone.now()
+        # group.is_high_risk = False
+        # group.status_updated = None
+        # group.tagline = ""
+        # group.save()
+
+        return Response({"message": "Group deleted successfully."}, status.HTTP_200_OK)
 
 
 class GroupViewSet(viewsets.ModelViewSet[Group]):
