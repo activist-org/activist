dbd16f9c06aeff6a01fdb163529965c55093ddb5
tosta <tobias.stach@gmail.com>
2025-05-09 20:02:44 +0200

    fixed query on the resource retrieve method

--- backend/content/tests/test_resource_retrieve.py
+++ backend/content/tests/test_resource_retrieve.py
@@ -1,39 +1,38 @@
-# # SPDX-License-Identifier: AGPL-3.0-or-later
-# import pytest
-# from rest_framework.test import APIClient
+# SPDX-License-Identifier: AGPL-3.0-or-later
+import pytest
+from rest_framework.test import APIClient
 
-# from authentication.factories import UserFactory
-# from content.factories import ResourceFactory
+from authentication.factories import UserFactory
+from content.factories import ResourceFactory
 
-# pytestmark = pytest.mark.django_db
+pytestmark = pytest.mark.django_db
 
 
-# def test_resource_retrieve():
-#     client = APIClient()
+def test_resource_retrieve():
+    client = APIClient()
 
-#     test_user = "test_user"
-#     test_pass = "test_pass"
-#     user = UserFactory(
-#         username=test_user,
-#         plaintext_password=test_pass,
-#         is_confirmed=True,
-#         verified=True,
-#     )
+    test_user = "test_user"
+    test_pass = "test_pass"
+    UserFactory(
+        username=test_user,
+        plaintext_password=test_pass,
+        is_confirmed=True,
+        verified=True,
+    )
 
-#     resource = ResourceFactory.build(is_private=False)
+    resource = ResourceFactory(is_private=False)
 
-#     # login = client.post(
-#     #     path="/v1/auth/sign_in/",
-#     #     data={"username": user.username, "password": user.password},
-#     # )
+    # User login
+    login = client.post(
+        path="/v1/auth/sign_in/", data={"username": test_user, "password": test_pass}
+    )
 
-#     # assert login.status_code == 200
-#     # login_body = login.json()
-#     # token = login_body["token"]
+    assert login.status_code == 200
+    login_body = login.json()
+    token = login_body["token"]
 
-#     # client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
-#     response = client.get(path=f"/v1/content/resources/{resource.id}/")
+    # Passing authorization header.
+    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+    response = client.get(path=f"/v1/content/resources/{resource.id}/")
 
-#     assert response.status_code == 200
-#     body = response.json()
-#     print(body)
+    assert response.status_code == 200
