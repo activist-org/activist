1265fc0a5039e5d15f2a81f828dda75f712fd5f3
nicky <Nicky.stru@gmail.com>
2025-05-17 13:28:46 +0300

    fix bound fixes

--- frontend/pages/home/index.vue
+++ frontend/pages/home/index.vue
@@ -1,6 +1,6 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 <template>
-  <div id="home-page-main" class="bg-layer-0 px-4 text-primary-text xl:px-8">
+  <div id="home-page-main" class="bg-layer-0 px-4 text-primary-text xl:px-8 pb-4">
     <Head>
       <Title>{{ $t("i18n._global.home") }}</Title>
     </Head>
