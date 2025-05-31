288705a686f0dcb087bbbdf7663cb33d9ca35b3b
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-05-31 20:15:14 +0200

    Update pre-commit to run fe checks only on staged files

--- pre_commit_scripts/prettier_eslint_checks.py
+++ pre_commit_scripts/prettier_eslint_checks.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """
-pre-commit hook for running ESLint static type checks.
+pre-commit hook for checking Prettier formatting.
 """
 
 import os
@@ -13,4 +13,4 @@ if not sys.platform.startswith("win"):
     run_shell_command("corepack enable")
 
 run_shell_command("yarn install")
-run_shell_command("yarn lint")
+run_shell_command("yarn lint-staged")
