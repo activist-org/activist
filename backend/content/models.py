d71f1839a54357dfc8f8aff66b987e240780583a
hericlibong <heric.afrimages@gmail.com>
2025-06-19 16:44:26 +0200

    fix(mypy): add missing type annotation for 'flags' in Resource model

--- backend/content/models.py
+++ backend/content/models.py
@@ -11,6 +11,7 @@ from django.contrib.postgres.fields import ArrayField
 from django.core.validators import validate_image_file_extension
 from django.db import models
 from django.db.models.signals import post_delete
+# from django.db.models.manager import RelatedManager
 from django.dispatch import receiver
 
 from utils.models import ISO_CHOICES
@@ -177,10 +178,10 @@ class Resource(models.Model):
     tags = models.ManyToManyField("content.Tag", blank=True)
     topics = models.ManyToManyField("content.Topic", blank=True)
 
-    flags = models.ManyToManyField(
-        "authentication.UserModel",
-        through="ResourceFlag",
-    )
+    flags: Any = models.ManyToManyField(
+    "authentication.UserModel",
+    through="ResourceFlag",
+)
 
     def __str__(self) -> str:
         return self.name
