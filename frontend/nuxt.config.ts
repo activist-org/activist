5c34821cadb2395059f2054db81347e4bdaf3319
sidhartthhhhh <singhnishant294@gmail.com>
2025-07-14 20:36:10 +0530

    chore: restore yarn.lock to match main

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import type { NuxtPage } from "nuxt/schema";
-
 import { resolve } from "path";
 
 import applyMiddleware from "./applyMiddleware";
