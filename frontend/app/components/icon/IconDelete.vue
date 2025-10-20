2dc0ff38f4b0d0d25ee8b0e0d554ab8fa30e7d59
Omkar Sathish <omkarsathish06@gmail.com>
2025-10-20 15:40:55 +0530

    feat: added icon

---
+++ frontend/app/components/icon/IconDelete.vue
@@ -0,0 +1,18 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<template>
+  <div
+    class="cursor-pointer text-primary-text hover:text-action-red"
+    data-testid="icon-delete"
+    v-bind="$attrs"
+  >
+    <Icon :name="IconMap.TRASH" size="1.2em" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IconMap } from "~/types/icon-map";
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
