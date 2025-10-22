c90f89f05f7e9b10fd82b81e2e1a3aecd051f742
nicki182 <Nicky.stru@gmail.com>
2025-10-22 11:18:33 +0300

    chore(frontend):added unique test identifier for icon edit draggable component

--- frontend/app/components/icon/IconDraggableEdit.vue
+++ frontend/app/components/icon/IconDraggableEdit.vue
@@ -3,7 +3,7 @@
   <div
     v-if="canEditEntity"
     class="cursor-pointer text-primary-text hover:text-distinct-text"
-    data-testid="icon-edit"
+    data-testid="icon-edit-draggable"
   >
     <Icon :name="IconMap.GRIP" :size="size ?? '1.2em'" />
   </div>
