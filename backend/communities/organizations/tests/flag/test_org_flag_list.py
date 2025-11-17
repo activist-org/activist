50f465eadfc47da3767e076799e53e5277545966
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-11-17 12:30:54 +0100

    initial refactor of communities tests

--- backend/communities/organizations/tests/flag/test_org_flag_list.py
+++ backend/communities/organizations/tests/flag/test_org_flag_list.py
@@ -1,35 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import pytest
-from rest_framework.test import APIClient
-
-from authentication.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
 
 
-def test_org_flag_list():
+def test_org_flag_list(authenticated_client):
     """
     Test to list all organization flags.
     """
-    client = APIClient()
-
-    test_username = "username"
-    test_password = "password"
-    user = UserFactory(username=test_username, plaintext_password=test_password)
-    user.is_confirmed = True
-    user.verified = True
-    user.save()
-
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_password},
-    )
-
-    assert login.status_code == 200
-
-    login_body = login.json()
-    token = login_body["access"]
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+    client, user = authenticated_client
     response = client.get(path="/v1/communities/organization_flags")
 
     assert response.status_code == 200
--- backend/communities/organizations/tests/resource/test_org_resource_create.py
+++ backend/communities/organizations/tests/resource/test_org_resource_create.py
@@ -1,9 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import pytest
-from rest_framework.test import APIClient
 
-from authentication.factories import UserFactory
 from communities.organizations.factories import (
     OrganizationFactory,
     OrganizationResourceFactory,
@@ -14,16 +12,8 @@ from content.models import Topic
 pytestmark = pytest.mark.django_db
 
 
-def test_org_resource_create_200():
-    client = APIClient()
-
-    test_username = "test_user"
-    test_password = "test_pass"
-    user = UserFactory(username=test_username, plaintext_password=test_password)
-    user.is_confirmed = True
-    user.verified = True
-    user.is_staff = True
-    user.save()
+def test_org_resource_create_200(authenticated_client):
+    client, user = authenticated_client
 
     org = OrganizationFactory(created_by=user)
     resource = OrganizationResourceFactory(created_by=user, org=org)
@@ -33,18 +23,6 @@ def test_org_resource_create_200():
     test_desc = resource.description
     test_url = resource.url
     test_order = resource.order
-
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_password},
-    )
-
-    assert login.status_code == 200
-    login_body = login.json()
-
-    token = login_body["access"]
-
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
 
     response = client.post(
         path="/v1/communities/organization_resources",
@@ -64,15 +42,8 @@ def test_org_resource_create_200():
     assert response_body["message"] == "Resource created successfully."
 
 
-def test_org_resource_create_403():
-    client = APIClient()
-
-    test_username = "test_user"
-    test_password = "test_pass"
-    user = UserFactory(username=test_username, plaintext_password=test_password)
-    user.is_confirmed = True
-    user.verified = True
-    user.save()
+def test_org_resource_create_403(authenticated_client):
+    client, user = authenticated_client
 
     org = OrganizationFactory()
     resource = OrganizationResourceFactory(created_by=user, org=org)
@@ -82,18 +53,6 @@ def test_org_resource_create_403():
     test_desc = resource.description
     test_url = resource.url
     test_order = resource.order
-
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_password},
-    )
-
-    assert login.status_code == 200
-    login_body = login.json()
-
-    token = login_body["access"]
-
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
 
     response = client.post(
         path="/v1/communities/organization_resources",
