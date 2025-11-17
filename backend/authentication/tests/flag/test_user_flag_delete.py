752e04c962aa045f904cefca47bf11afc2cc02cf
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-11-17 14:53:46 +0100

    finished refactoring test for authenticated users

--- backend/authentication/tests/flag/test_user_flag_delete.py
+++ backend/authentication/tests/flag/test_user_flag_delete.py
@@ -3,27 +3,19 @@ import logging
 from uuid import uuid4
 
 import pytest
-from rest_framework.test import APIClient
 
-from authentication.factories import UserFactory, UserFlagFactory
+from authentication.factories import UserFlagFactory
 
 logger = logging.getLogger(__name__)
 pytestmark = pytest.mark.django_db
 
 
-def test_user_flag_delete():
+def test_user_flag_delete(authenticated_client):
     """
     Test to delete a flag of a user.
     """
     logger.info("Starting test_user_flag_delete")
-    client = APIClient()
-
-    test_username = "username"
-    test_pass = "password"
-    logger.debug(f"Creating test user with username: {test_username}")
-    user = UserFactory(username=test_username, plaintext_password=test_pass)
-    user.is_confirmed = True
-    user.verified = True
+    client, user = authenticated_client
     user.is_staff = True
     user.save()
     logger.debug(f"Test user created with ID: {user.id}")
@@ -32,19 +24,6 @@ def test_user_flag_delete():
     flagged_user = UserFlagFactory()
     logger.debug(f"Flagged user created with ID: {flagged_user.id}")
 
-    # Login to get token.
-    logger.debug("Attempting user login to get authentication token")
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_pass},
-    )
-
-    assert login.status_code == 200
-    logger.debug("User login successful")
-
-    login_body = login.json()
-    token = login_body["access"]
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
     logger.debug(f"Attempting to delete user flag with ID: {flagged_user.id}")
     response = client.delete(path=f"/v1/auth/user_flags/{flagged_user.id}")
 
