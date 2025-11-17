cbeeab47f4bf43788f3375ff8bb82eb84823f8e8
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-11-17 14:34:24 +0100

    refactored event tests

--- backend/events/tests/resource/test_event_resource_create.py
+++ backend/events/tests/resource/test_event_resource_create.py
@@ -1,25 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import pytest
-from rest_framework.test import APIClient
 
-from authentication.factories import UserFactory
 from events.factories import EventFactory, EventResourceFactory
 
 pytestmark = pytest.mark.django_db
 
 
-def test_event_resource_create_200():
-    client = APIClient()
-
-    test_username = "test_user"
-    test_password = "test_pass"
-    user = UserFactory(username=test_username, plaintext_password=test_password)
-    user.is_confirmed = True
-    user.verified = True
-    user.is_staff = True
-    user.save()
-
+def test_event_resource_create_200(authenticated_client):
+    client, user = authenticated_client
     event = EventFactory(created_by=user)
 
     resource = EventResourceFactory(created_by=user, event_id=event.id)
@@ -27,18 +16,6 @@ def test_event_resource_create_200():
     test_desc = resource.description
     test_url = resource.url
     test_order = resource.order
-
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_password},
-    )
-
-    assert login.status_code == 200
-    login_body = login.json()
-
-    token = login_body["access"]
-
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
 
     response = client.post(
         path="/v1/events/event_resources",
@@ -57,15 +34,8 @@ def test_event_resource_create_200():
     assert response_body["message"] == "Resource created successfully."
 
 
-def test_event_resource_create_403():
-    client = APIClient()
-
-    test_username = "test_user"
-    test_password = "test_pass"
-    user = UserFactory(username=test_username, plaintext_password=test_password)
-    user.is_confirmed = True
-    user.verified = True
-    user.save()
+def test_event_resource_create_403(authenticated_client):
+    client, user = authenticated_client
 
     event = EventFactory()
 
@@ -74,18 +44,6 @@ def test_event_resource_create_403():
     test_desc = resource.description
     test_url = resource.url
     test_order = resource.order
-
-    login = client.post(
-        path="/v1/auth/sign_in",
-        data={"username": test_username, "password": test_password},
-    )
-
-    assert login.status_code == 200
-    login_body = login.json()
-
-    token = login_body["access"]
-
-    client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
 
     response = client.post(
         path="/v1/events/event_resources",
