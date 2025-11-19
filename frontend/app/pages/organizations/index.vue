ae5275e377972d4b32602507d1e94859ca1613f6
nicki182 <Nicky.stru@gmail.com>
2025-11-20 00:14:03 +0200

    fix(frontend):use correct loading component when loading new organizations

--- frontend/app/pages/organizations/index.vue
+++ frontend/app/pages/organizations/index.vue
@@ -30,9 +30,7 @@
 
       <!-- sentinel -->
       <div ref="bottomSentinel">
-        <h1 v-if="loadingFetchMore && pending">
-Loading...
-</h1>
+        <Loading v-if="loadingFetchMore && pending" />
       </div>
     </div>
 
