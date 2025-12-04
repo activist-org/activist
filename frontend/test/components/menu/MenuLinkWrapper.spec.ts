f8ece4fec31422bf3a5ced85f5dad2cb5b3ad0c6
mfahad2 <mfahad@andrew.cmu.edu>
2025-12-04 16:04:36 +0300

    fixing license issue

--- frontend/test/components/menu/MenuLinkWrapper.spec.ts
+++ frontend/test/components/menu/MenuLinkWrapper.spec.ts
@@ -1,5 +1,5 @@
-import { screen } from "@testing-library/vue";
 // SPDX-License-Identifier: AGPL-3.0-or-later
+import { screen } from "@testing-library/vue";
 import { describe, expect, it } from "vitest";
 
 import MenuLinkWrapper from "../../../app/components/menu/MenuLinkWrapper.vue";
