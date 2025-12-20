51e54955eeee54b19c60dd53c02578938d33fcef
nicki182 <Nicky.stru@gmail.com>
2025-12-21 00:12:56 +0200

    fix:verify header cheker

--- frontend/test-e2e/specs/desktop/faq-keyboard-navigation-permissions.spec.ts
+++ frontend/test-e2e/specs/desktop/faq-keyboard-navigation-permissions.spec.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 import { navigateToFirstEvent } from "~/test-e2e/actions/navigation";
 import { expect, test } from "~/test-e2e/global-fixtures";
 import { newEventPage } from "~/test-e2e/page-objects/event/EventPage";
