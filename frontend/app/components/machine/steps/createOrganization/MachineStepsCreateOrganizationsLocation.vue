a5915b03f682371bbdeb8af39d9723fd5fa16120
Nicole <31940739+nicki182@users.noreply.github.com>
2025-12-20 15:39:07 +0200

    WIP Create architecture machine, machines for create flow and modals (#1789)
    * chore(frontend): created flow and machines within stores and composable to interpret useFlowScreens
    
    * chore(frontend): created history prop, added flow logic in one source of truth
    
    * chore(frontend): some refinements on the machine logic
    
    * chore: fixed typing and added create event modal draft
    
    * chore: added new screen event type and roles
    
    * chore: added new screen event location and dates
    
    * chore: added missing modals and fixed issue with initial loading of screens
    
    * chore: added previus option for modals and added models create group and create organization for user
    
    * chore: fix typing for create-event flow store
    
    * fix: removed unnecesary create and fix prev
    
    * chore: added new filter of groups and organizaions
    
    * chore:created new selector component to select countries
    
    * chore: schedule screen is being finished, removed unnecesary screen and added manual logic steps for more complex steps system
    
    * chore: change progress bar color and fix infinite scroll for orgaanizations
    
    * chore: added disabled in FormCombobox
    
    * chore: remove console logs in frontend
    
    * Ruff formatting/linting fixes + license headers
    
    * Code formatting and linting fixes
    
    * Final prettier fixes and license header fixes
    
    * Add all necessary i18n keys and fixes for them
    
    * Remove unneeded files and fix mypy error
    
    * Fixes to TS errors, comment improvements and misc fixes
    
    * Fix Vitest tests including changes given paginated groups endpoint
    
    * Fix prettier formatting
    
    * Fix Vitest test
    
    ---------
    
    Co-authored-by: Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>

---
+++ frontend/app/components/machine/steps/createOrganization/MachineStepsCreateOrganizationsLocation.vue
@@ -0,0 +1,72 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<template>
+  <div class="px-4 sm:px-6 md:px-8 xl:px-24 2xl:px-36">
+    <Form
+      id="event-location"
+      @submit="handleSubmit"
+      :action-buttons="[
+        {
+          onclick: handlePrev,
+          cta: false,
+          fontSize: 'base',
+          ariaLabel:
+            'i18n.components.machine.steps._global.previous_step_aria_label',
+          label: 'i18n.components.machine.steps._global.previous_step',
+          type: 'button',
+        },
+      ]"
+      class="space-y-4"
+      :schema="locationSchema"
+    >
+      <FormItem
+        v-slot="{ id, handleChange, errorMessage, value }"
+        :label="$t('i18n.components.machine.steps._global.country')"
+        name="country"
+      >
+        <!-- prettier-ignore-attribute :modelValue -->
+        <FormSelectorComboboxCountry
+          :id="id"
+          @update:selected-country="handleChange"
+          :hasError="!!errorMessage.value"
+          :label="$t('i18n.components.machine.steps._global.country')"
+          :selected-country="(value.value as string) || ''"
+        />
+      </FormItem>
+      <FormItem
+        v-slot="{ id, handleChange, handleBlur, errorMessage, value }"
+        :label="$t('i18n.components.machine.steps._global.city')"
+        name="city"
+      >
+        <!-- prettier-ignore-attribute :modelValue -->
+        <FormTextInput
+          :id="id"
+          @blur="handleBlur"
+          @input="handleChange"
+          :hasError="!!errorMessage.value"
+          :label="$t('i18n.components.machine.steps._global.city')"
+          :modelValue="(value.value as string)"
+        />
+      </FormItem>
+    </Form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { z } from "zod";
+
+const flow = inject<FlowControls>("flow");
+const locationSchema = z.object({
+  country: z.string().min(1, "Country is required"),
+  city: z.string().min(1, "City is required"),
+});
+const handlePrev = () => {
+  if (!flow) return;
+  flow.prev();
+};
+const handleSubmit = async (values: Record<string, unknown>) => {
+  // Simulate an API call.
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  if (!flow) return;
+  flow.next(values);
+};
+</script>
