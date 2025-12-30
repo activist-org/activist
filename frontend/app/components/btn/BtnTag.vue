273f1bbecb92153476cfc9dcd505849271649def
nicki182 <Nicky.stru@gmail.com>
2025-12-30 11:15:57 +0200

    fix: build

--- frontend/app/components/btn/BtnTag.vue
+++ frontend/app/components/btn/BtnTag.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import type { BtnAction } from "#shared/types/btn-props";
+import type { BtnAction } from "#shared/types/components-props";
 
 const props = defineProps<BtnAction>();
 const btnDynamicClass = computed(() =>
