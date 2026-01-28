0d185fa0b7412a8cf756fb84141cea5845f0570a
Mariakourbi <mkourm@csd.auth.gr>
2026-01-28 13:28:29 +0200

    chore(test): fix lint and add license header
    Added copyright notice and updated import statement.

--- frontend/test/app/components/footer/FooterWebsite.spec.ts
+++ frontend/test/app/components/footer/FooterWebsite.spec.ts
@@ -1,9 +1,15 @@
-/// <reference types="vitest" />
+
+/*
+ * Copyright (c) 2025 activist
+ *
+ * This Source Code Form is subject to the terms of the
+ * GNU Affero General Public License, version 3.
+ */
 
 import { describe, it, expect } from "vitest";
-import  render  from "../../../render";
 
 import FooterWebsite from "../../../../app/components/footer/FooterWebsite.vue";
+import  {render]  from "../../../render";
 
 describe("FooterWebsite", () => {
   it("renders without crashing", async () => {
