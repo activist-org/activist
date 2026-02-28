cf25eb1800a7fddc4a9f069ec2f90ad8fee4f4fa
Memeske <petz2002@gmail.com>
2026-02-28 02:10:20 +0100

    fix: resolved undefined organization name on event cards

--- frontend/app/components/meta-tag/MetaTagOrganization.vue
+++ frontend/app/components/meta-tag/MetaTagOrganization.vue
@@ -1,22 +1,23 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 <template>
   <NuxtLink
+    v-if="organization && organization.length > 0"
     :aria-label="
       $t('i18n.components._global.navigate_to_organization_aria_label')
     "
-    :to="$localePath(`/organizations/${organization.id}/about`)"
+    :to="$localePath(`/organizations/${organization[0]?.id}/about`)"
   >
     <MetaTag
       class="pr-2"
       iconName="IconOrganization"
-      :value="organization.name"
+      :value="organization[0]?.name ?? ''"
     />
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-  organization: Organization;
+  organization: Organization[];
   iconSize?: string;
 }>();
 </script>
