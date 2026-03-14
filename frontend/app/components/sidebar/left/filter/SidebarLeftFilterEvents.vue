953930bc67c17b3f6877e0a413871b9b5e1d7e90
Nicole <31940739+nicki182@users.noreply.github.com>
2026-03-14 23:36:07 +0200

    fix(backend): fix dummy data creation closer to new model (#2006)
    * fix(backend): fix dummy data creation closer to new model
    
    * fixes on country code and adjusting filters
    
    * fix in prettier frontend
    
    * Minor code formatting
    
    * fix: event creation for location type
    
    * fix: imports
    
    * Minor update to formatting and change dummy url
    
    ---------
    
    Co-authored-by: Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>

--- frontend/app/components/sidebar/left/filter/SidebarLeftFilterEvents.vue
+++ frontend/app/components/sidebar/left/filter/SidebarLeftFilterEvents.vue
@@ -55,7 +55,7 @@
         v-slot="{ id, handleChange, value }"
         data-testid="events-filter-location-type"
         :label="$t('i18n.components._global.location_type')"
-        name="setting"
+        name="locationType"
       >
         <!-- prettier-ignore-attribute :modelValue -->
         <FormSelectorRadio
@@ -127,7 +127,7 @@ const schema = z.object({
   location: z.string().optional(),
   topics: z.array(z.string()).optional(),
   type: z.string().optional(),
-  setting: z.string().optional(),
+  locationType: z.string().optional(),
   viewType: z.string().optional(),
 });
 const sidebar = useSidebar();
