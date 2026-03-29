1b9c6404b0421e23c83b45956c4d96d8f407b023
Matt Burnett <mburnett@tutanota.de>
2026-03-29 18:23:04 -0400

    Fix(issue_1171): Unneeded SPDX headers.
    - Removed unneeded SPDX headers from __init__.py files that had them.

--- backend/core/filescan/__init__.py
+++ backend/core/filescan/__init__.py
@@ -1,4 +1,3 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
 """
 Filescan client package. Re-export the public API.
 """
--- services/filescan/tests_integration/__init__.py
+++ services/filescan/tests_integration/__init__.py
@@ -1,4 +1,3 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
 """
 Integration test package for the filescan service.
 """
