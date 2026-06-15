376951d9fafac8f390b3e1a559bc2d5a073c6959
Atharv Hatwar <atharvhatwar02@gmail.com>
2026-06-15 10:01:03 +0530

    fix(test): restore SPDX license headers to integration specs

--- frontend/test/composables/queries/integration/useGetEvents.integration.spec.ts
+++ frontend/test/composables/queries/integration/useGetEvents.integration.spec.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 /* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-explicit-any */
 import { mockNuxtImport } from "@nuxt/test-utils/runtime";
 import { createPinia, setActivePinia } from "pinia";
