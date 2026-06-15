4b46aaaa0807d477b5de937289807f15e1419f40
Basit ur Rehman Malik <bm25abc@herts.ac.uk>
2026-06-15 16:02:37 +0100

    revert(frontend): restore aria-labelledby and hidden span for a11y as requested

--- frontend/app/components/icon/IconDelete.vue
+++ frontend/app/components/icon/IconDelete.vue
@@ -2,11 +2,13 @@
 <template>
   <button
     v-if="canDeleteEntity"
-    :aria-label="$t('i18n.components.icon_delete.aria_label_alt_text')"
+    aria-labelledby="icon-delete-label"
     class="cursor-pointer text-action-red focus-brand hover:text-distinct-text"
     data-testid="icon-delete"
   >
     <Icon :name="IconMap.TRASH" size="1.2em" />
+    <!-- Note: Buttons need text for a11y. Adding the following for e2e tests. -->
+    <span id="icon-delete-label" hidden>{{ $t("i18n.components.icon_delete.aria_label_alt_text") }}</span>
   </button>
 </template>
 
